8b07eeb9fe0109ade8ca16884da4a5731b60f0f6
TianJin <tfanxi@163.com>
2017-05-10 16:50:04 +0800

    add statistics member module content

--- resources/mixes/administration/src/pages/StatisticsMember.vue
+++ resources/mixes/administration/src/pages/StatisticsMember.vue
@@ -61,27 +61,17 @@
                         rate: 2,
                     },
                 ],
-                goodsList: [
-                    {
-                        value: '1',
-                        label: '商品1',
-                    },
-                    {
-                        value: '2',
-                        label: '商品2',
-                    },
-                ],
-                goodsColumns: [
+                analysisColumns: [
                     {
                         title: '序号',
                         key: 'num',
                     },
                     {
-                        title: '商品名称',
-                        key: 'goodsName',
+                        title: '会员名称',
+                        key: 'memberName',
                     },
                     {
-                        title: '下单商品数',
+                        title: '下单量',
                         key: 'amount',
                     },
                     {
@@ -93,39 +83,43 @@
                         },
                     },
                 ],
-                goodsData: [
+                analysisData: [
                     {
-                        num: 1,
-                        goodsName: 'xxx旗舰店',
-                        amount: 99,
+                        num: 333,
+                        memberName: 4,
+                        amount: 22,
                     },
                     {
-                        num: 2,
-                        goodsName: 'xxx旗舰店',
-                        amount: 99,
+                        num: 333,
+                        memberName: 4,
+                        amount: 22,
                     },
                     {
-                        num: 3,
-                        goodsName: 'xxx旗舰店',
-                        amount: 99,
+                        num: 333,
+                        memberName: 4,
+                        amount: 22,
                     },
                     {
-                        num: 4,
-                        goodsName: 'xxx旗舰店',
-                        amount: 99,
+                        num: 333,
+                        memberName: 4,
+                        amount: 22,
                     },
                 ],
-                shopsColumns: [
+                provinceColumns: [
                     {
-                        title: '序号',
-                        key: 'num',
+                        title: '省份',
+                        key: 'province',
                     },
                     {
-                        title: '商品名称',
-                        key: 'goodsName',
+                        title: '下单会员数',
+                        key: 'memberNum',
                     },
                     {
-                        title: '下单商品数',
+                        title: '下单金额（元）',
+                        key: 'money',
+                    },
+                    {
+                        title: '下单量',
                         key: 'amount',
                     },
                     {
@@ -137,26 +131,30 @@
                         },
                     },
                 ],
-                shopsData: [
+                provinceData: [
                     {
-                        num: 1,
-                        goodsName: 'xxx旗舰店',
-                        amount: 99,
+                        province: '陕西',
+                        memberNum: 4,
+                        money: 222,
+                        amount: 20,
                     },
                     {
-                        num: 2,
-                        goodsName: 'xxx旗舰店',
-                        amount: 99,
+                        province: '陕西',
+                        memberNum: 4,
+                        money: 222,
+                        amount: 20,
                     },
                     {
-                        num: 3,
-                        goodsName: 'xxx旗舰店',
-                        amount: 99,
+                        province: '陕西',
+                        memberNum: 4,
+                        money: 222,
+                        amount: 20,
                     },
                     {
-                        num: 4,
-                        goodsName: 'xxx旗舰店',
-                        amount: 99,
+                        province: '陕西',
+                        memberNum: 4,
+                        money: 222,
+                        amount: 20,
                     },
                 ],
                 timeList: [
@@ -177,6 +175,16 @@
             exportData() {
                 this.$refs.memberList.exportCsv({
                     filename: '新增会员数据',
+                });
+            },
+            exportMemberData() {
+                this.$refs.analysisList.exportCsv({
+                    filename: '会员分析数据',
+                });
+            },
+            exportProvinceData() {
+                this.$refs.provinceList.exportCsv({
+                    filename: '地区分析数据',
                 });
             },
         },
@@ -233,69 +241,44 @@
                     <card :bordered="false">
                         <div class="prompt-box">
                             <p>提示</p>
-                            <p>在“提示”下可以选择不同的商品分类和时间查询数据</p>
-                            <p>统计某行业在不同时间段下单量前50名商品和前30名店铺</p>
+                            <p>符合以下任何一种条件的订单即为有效订单：1、采用在线支付方式支付并且已付款；
+                                2、采用货到付款方式支付并且交易已完成</p>
+                            <p>列表展示了时间段内所有会员有效订单的订单数量、下单商品数量和订单总金额统计数据，
+                                并可以点击列表上方的"导出数据"，将列表数据导出为Excel文件</p>
                         </div>
                         <div class="analysis-content">
-                            <h5 v-if="!isPriceArea">行业商品TOP50</h5>
-                            <div class="order-money-content">
-                                <div class="select-content" style="top: -10px">
-                                    <ul>
-                                        <li>
-                                            商品分类
-                                            <i-select v-model="model2" style="width:124px">
-                                                <i-option v-for="item in goodsList" :value="item.value"
-                                                          :key="item">{{ item.label }}</i-option>
-                                            </i-select>
-                                        </li>
-                                        <li>
-                                            时间周期
-                                            <i-select v-model="model2" style="width:124px">
-                                                <i-option v-for="item in timeList" :value="item.value"
-                                                          :key="item">{{ item.label }}</i-option>
-                                            </i-select>
-                                        </li>
-                                        <li>
-                                            <date-picker type="date" placeholder="选择日期"></date-picker>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div style="height: 350px">
+                            <tabs type="card">
+                                <tab-pane label="下单金额">
+                                    <div class="order-money-content">
+                                        <h5>买家排行榜TOP15</h5>
+                                        <div class="select-content">
+                                            <ul>
+                                                <li>
+                                                    时间周期
+                                                    <i-select v-model="model2" style="width:124px">
+                                                        <i-option v-for="item in timeList" :value="item.value"
+                                                                  :key="item">{{ item.label }}</i-option>
+                                                    </i-select>
+                                                </li>
+                                                <li>
+                                                    <date-picker type="date" placeholder="选择日期"></date-picker>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <div style="height: 350px">
 
-                                </div>
-                                <i-table :columns="goodsColumns" :context="self" :data="goodsData"></i-table>
-                                <div class="page">
-                                    <page :total="100" show-elevator></page>
-                                </div>
-                            </div>
-                            <h5 v-if="!isPriceArea">行业店铺TOP50</h5>
-                            <div class="order-money-content">
-                                <div class="select-content" style="top: -10px">
-                                    <ul>
-                                        <li>
-                                            商品分类
-                                            <i-select v-model="model2" style="width:124px">
-                                                <i-option v-for="item in goodsList" :value="item.value"
-                                                          :key="item">{{ item.label }}</i-option>
-                                            </i-select>
-                                        </li>
-                                        <li>
-                                            时间周期
-                                            <i-select v-model="model2" style="width:124px">
-                                                <i-option v-for="item in timeList" :value="item.value"
-                                                          :key="item">{{ item.label }}</i-option>
-                                            </i-select>
-                                        </li>
-                                        <li>
-                                            <date-picker type="date" placeholder="选择日期"></date-picker>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div style="height: 350px">
-
-                                </div>
-
-                            </div>
+                                        </div>
+                                        <i-button type="ghost" class="export-btn" @click="exportMemberData">导出数据</i-button>
+                                        <i-table :columns="analysisColumns" :context="self"
+                                                 :data="analysisData" ref="analysisList"></i-table>
+                                        <div class="page">
+                                            <page :total="100" show-elevator></page>
+                                        </div>
+                                    </div>
+                                </tab-pane>
+                                <tab-pane label="下单商品数">标签二的内容</tab-pane>
+                                <tab-pane label="下单量">标签三的内容</tab-pane>
+                            </tabs>
                         </div>
                     </card>
                 </tab-pane>
@@ -309,35 +292,37 @@
                             <p>统计图展示符合搜索条件的有效订单中商品的单价所在价格区间中的总销售额和总下单商品数量走势</p>
                         </div>
                         <div class="analysis-content">
-                            <i-button type="ghost">设置价格区间</i-button>
-                            <h5>行业价格分布图</h5>
-                            <div class="order-money-content">
-                                <h5 v-if="!isPriceArea">下单金额</h5>
-                                <div class="select-content" style="top: -10px">
-                                    <ul>
-                                        <li>
-                                            商品分类
-                                            <i-select v-model="model2" style="width:124px">
-                                                <i-option v-for="item in goodsList" :value="item.value"
-                                                          :key="item">{{ item.label }}</i-option>
-                                            </i-select>
-                                        </li>
-                                        <li>
-                                            时间周期
-                                            <i-select v-model="model2" style="width:124px">
-                                                <i-option v-for="item in timeList" :value="item.value"
-                                                          :key="item">{{ item.label }}</i-option>
-                                            </i-select>
-                                        </li>
-                                        <li>
-                                            <date-picker type="date" placeholder="选择日期"></date-picker>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div v-if="isPriceArea" class="set-price-area">
-                                    <p>看行业价格分布情况前，请先设置价格区间</p>
-                                </div>
-                            </div>
+                            <tabs type="card">
+                                <tab-pane label="下单金额">
+                                    <div class="order-money-content">
+                                        <div class="select-content">
+                                            <ul>
+                                                <li>
+                                                    时间周期
+                                                    <i-select v-model="model2" style="width:124px">
+                                                        <i-option v-for="item in timeList" :value="item.value"
+                                                                  :key="item">{{ item.label }}</i-option>
+                                                    </i-select>
+                                                </li>
+                                                <li>
+                                                    <date-picker type="date" placeholder="选择日期"></date-picker>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <div style="height: 350px">
+
+                                        </div>
+                                        <i-button type="ghost" class="export-btn" @click="exportProvinceData">导出数据</i-button>
+                                        <i-table :columns="provinceColumns" :context="self"
+                                                 :data="provinceData" ref="provinceList"></i-table>
+                                        <div class="page">
+                                            <page :total="100" show-elevator></page>
+                                        </div>
+                                    </div>
+                                </tab-pane>
+                                <tab-pane label="下单商品数">标签二的内容</tab-pane>
+                                <tab-pane label="下单量">标签三的内容</tab-pane>
+                            </tabs>
                         </div>
                     </card>
                 </tab-pane>
