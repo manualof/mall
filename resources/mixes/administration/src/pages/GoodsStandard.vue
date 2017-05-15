f7a5bf18555d9a174f8006d5205a275e4c934759
TianJin <tfanxi@163.com>
2017-05-15 16:17:29 +0800

    add card component

--- resources/mixes/administration/src/pages/GoodsStandard.vue
+++ resources/mixes/administration/src/pages/GoodsStandard.vue
@@ -105,26 +105,27 @@
         <div class="goods-standard">
             <tabs value="name1">
                 <tab-pane label="规格管理" name="name1">
-                    <div class="prompt-box">
-                        <p>提示</p>
-                        <p>规格将会对应到商品发布的规格，规格值由店铺自己增加</p>
-                        <p>默认安装中会添加一个默认颜色规格，请不要删除，只有这个颜色规格才能在商品详细页显示为图片</p>
-                    </div>
-                    <div class="advertisement-action">
-                        <i-button class="add-data" type="ghost" @click.native="newAddData">+新增数据</i-button>
-                        <i-button type="text" icon="android-sync" class="refresh">刷新</i-button>
-                        <div class="goods-body-header-right">
-                            <i-input v-model="managementWord" placeholder="请输入关键词进行搜索">
-                                <i-select v-model="managementSearch" slot="prepend" style="width: 100px;">
-                                    <i-option v-for="item in searchList"
-                                              :value="item.value">{{ item.label }}</i-option>
-                                </i-select>
-                                <i-button slot="append" type="primary">搜索</i-button>
-                            </i-input>
+                    <card :bordered="false">
+                        <div class="prompt-box">
+                            <p>提示</p>
+                            <p>规格将会对应到商品发布的规格，规格值由店铺自己增加</p>
+                            <p>默认安装中会添加一个默认颜色规格，请不要删除，只有这个颜色规格才能在商品详细页显示为图片</p>
                         </div>
-                    </div>
-                    <i-table highlight-row :columns="typeColumns" :context="self"
-                             :data="typeData"></i-table>
+                        <div class="advertisement-action">
+                            <i-button class="add-data" type="ghost" @click.native="newAddData">+新增数据</i-button>
+                            <i-button type="text" icon="android-sync" class="refresh">刷新</i-button>
+                            <div class="goods-body-header-right">
+                                <i-input v-model="managementWord" placeholder="请输入关键词进行搜索">
+                                    <i-select v-model="managementSearch" slot="prepend" style="width: 100px;">
+                                        <i-option v-for="item in searchList"
+                                                  :value="item.value">{{ item.label }}</i-option>
+                                    </i-select>
+                                    <i-button slot="append" type="primary">搜索</i-button>
+                                </i-input>
+                            </div>
+                        </div>
+                        <i-table highlight-row :columns="typeColumns" :context="self" :data="typeData"></i-table>
+                    </card>
                 </tab-pane>
             </tabs>
         </div>
