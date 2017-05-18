a82f73baae78fea0e4feaff3abd96498aee5209b
TianJin <tfanxi@163.com>
2017-05-18 20:00:41 +0800

    modify map layoutSize

--- resources/mixes/administration/src/pages/StatisticsMember.vue
+++ resources/mixes/administration/src/pages/StatisticsMember.vue
@@ -261,20 +261,31 @@
                 orderMoneyProvince: {
                     tooltip: {},
                     visualMap: {
-                        min: 0,
-                        max: 1500,
-                        left: 'left',
-                        top: 'bottom',
-                        text: ['High', 'Low'],
-                        seriesIndex: [1],
+                        calculable: true,
                         inRange: {
                             color: ['#e0ffff', '#006edd'],
                         },
-                        calculable: true,
+                        left: 'left',
+                        max: 1500,
+                        min: 0,
+                        seriesIndex: [1],
+                        text: ['High', 'Low'],
+                        top: 'bottom',
                     },
                     geo: {
-                        map: 'china',
-                        roam: true,
+                        itemStyle: {
+                            emphasis: {
+                                areaColor: null,
+                                borderWidth: 0,
+                                shadowBlur: 20,
+                                shadowColor: 'rgba(0, 0, 0, 0.5)',
+                                shadowOffsetX: 0,
+                                shadowOffsetY: 0,
+                            },
+                            normal: {
+                                borderColor: 'rgba(0, 0, 0, 0.2)',
+                            },
+                        },
                         label: {
                             normal: {
                                 show: true,
@@ -283,52 +294,40 @@
                                 },
                             },
                         },
-                        itemStyle: {
-                            normal: {
-                                borderColor: 'rgba(0, 0, 0, 0.2)',
-                            },
-                            emphasis: {
-                                areaColor: null,
-                                shadowOffsetX: 0,
-                                shadowOffsetY: 0,
-                                shadowBlur: 20,
-                                borderWidth: 0,
-                                shadowColor: 'rgba(0, 0, 0, 0.5)',
-                            },
-                        },
+                        layoutCenter: ['50%', '50%'],
+                        layoutSize: 520,
+                        map: 'china',
+                        roam: true,
                     },
                     series: [
                         {
-                            type: 'scatter',
                             coordinateSystem: 'geo',
                             data: this.convertData(),
-                            symbolSize: 20,
+                            itemStyle: {
+                                normal: {
+                                    color: '#F06C00',
+                                },
+                            },
+                            label: {
+                                emphasis: {
+                                    show: true,
+                                },
+                                normal: {
+                                    formatter: '{b}',
+                                    position: 'right',
+                                    show: false,
+                                },
+                            },
                             symbol: 'path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.' +
                             '629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,' +
                             '221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,' +
                             '42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,' +
                             '1318.313z',
                             symbolRotate: 35,
-                            label: {
-                                normal: {
-                                    formatter: '{b}',
-                                    position: 'right',
-                                    show: false,
-                                },
-                                emphasis: {
-                                    show: true,
-                                },
-                            },
-                            itemStyle: {
-                                normal: {
-                                    color: '#F06C00',
-                                },
-                            },
+                            symbolSize: 80,
+                            type: 'scatter',
                         },
                         {
-                            name: '销量',
-                            type: 'map',
-                            geoIndex: 0,
                             data: [
                                 {
                                     name: '北京',
@@ -467,6 +466,9 @@
                                     value: this.randomValue(),
                                 },
                             ],
+                            geoIndex: 0,
+                            name: '下单金额',
+                            type: 'map',
                         },
                     ],
                 },
@@ -762,7 +764,8 @@
                                         </div>
                                     </div>
                                     <div class="echarts">
-                                        <i-echarts @click="onClick" :option="orderMoneyProvince" ref="echarts" @ready="onMapReady"></i-echarts>
+                                        <i-echarts @click="onClick" :option="orderMoneyProvince" ref="echarts"
+                                                   @ready="onMapReady"></i-echarts>
                                     </div>
                                     <i-button type="ghost" class="export-btn"
                                               @click="exportProvinceData">导出数据</i-button>
