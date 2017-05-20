836ffed0621cc07e0913ec393da4537a35dbacfb
twilroad <269044570@qq.com>
2017-05-20 17:15:55 +0800

    add entry classes for seller and user

---
+++ src/Controllers/Api/User/UserController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime 2017-05-20 17:10
+ */
+namespace Notadd\Mall\Controllers\Api\User;
+
+use Notadd\Foundation\Routing\Abstracts\Controller;
+
+/**
+ * Class UserController.
+ */
+class UserController extends Controller
+{
+}
