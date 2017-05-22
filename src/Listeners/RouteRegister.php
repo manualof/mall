470ae421803de2a1c21f7eb6b609c2ddd0c1211e
twilroad <269044570@qq.com>
2017-05-22 16:33:32 +0800

    add router, entry, and handler for statistics api

--- src/Listeners/RouteRegister.php
+++ src/Listeners/RouteRegister.php
@@ -34,6 +34,7 @@ use Notadd\Mall\Controllers\Api\Admin\ShopController;
 use Notadd\Mall\Controllers\Api\Admin\ShopDynamicController;
 use Notadd\Mall\Controllers\Api\Admin\ShopRateController;
 use Notadd\Mall\Controllers\Api\Admin\ProductSpecificationController;
+use Notadd\Mall\Controllers\Api\Admin\StatisticsController;
 use Notadd\Mall\Controllers\Api\Admin\UploadController;
 use Notadd\Mall\Controllers\Api\Admin\VirtualOrderController;
 use Notadd\Mall\Controllers\Api\Admin\VirtualProductController;
@@ -146,6 +147,7 @@ class RouteRegister extends AbstractRouteRegister
             $this->router->post('specification/edit', ProductSpecificationController::class . '@edit');
             $this->router->post('specification/list', ProductSpecificationController::class . '@list');
             $this->router->post('specification/remove', ProductSpecificationController::class . '@remove');
+            $this->router->post('statistics', StatisticsController::class . '@get');
             $this->router->post('upload', UploadController::class . '@handle');
             $this->router->post('virtual-order/create', VirtualOrderController::class . '@create');
             $this->router->post('virtual-order/edit', VirtualOrderController::class . '@edit');
