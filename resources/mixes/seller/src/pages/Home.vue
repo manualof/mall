cae2abf121a7b68ecc20affaa46f88cf1e8a8a22
TianJin <tfanxi@163.com>
2017-05-23 20:15:30 +0800

    add home content

--- resources/mixes/seller/src/pages/Home.vue
+++ resources/mixes/seller/src/pages/Home.vue
@@ -8,7 +8,17 @@
             });
         },
         data() {
-            return {};
+            return {
+                shop: {
+                    level: '白金店铺',
+                    manage: '管理员',
+                    name: '西部母婴用品店',
+                    valid: '2020年5月10日',
+                },
+                user: {
+                    name: 'nwaijhf',
+                },
+            };
         },
         methods: {},
     };
@@ -19,7 +29,24 @@
             <tabs value="home">
                 <tab-pane label="首页" name="home">
                     <card :bordered="false">
-
+                        <row class="top-shop-intro">
+                            <i-col class="img" span="2">
+                                <img src="" alt="">
+                            </i-col>
+                            <i-col span="22">
+                                <h4>{{ shop.name }}
+                                    <span>(用户名:{{ user.name }})</span>
+                                </h4>
+                                <ul>
+                                    <li>管理权限:{{ shop.manage }}</li>
+                                    <li>店铺等级:{{ shop.level }}</li>
+                                    <li>有效期:{{ shop.valid }}</li>
+                                </ul>
+                            </i-col>
+                        </row>
+                        <div class="bottom-shop-tip">
+                            
+                        </div>
                     </card>
                 </tab-pane>
             </tabs>
