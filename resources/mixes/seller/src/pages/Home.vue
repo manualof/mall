acc7cb5fc6b8b8ca50fb234e4507ee7c642af464
TianJin <tfanxi@163.com>
2017-05-23 18:32:54 +0800

    add home module

---
+++ resources/mixes/seller/src/pages/Home.vue
@@ -0,0 +1,28 @@
+<script>
+    import injection from '../helpers/injection';
+
+    export default {
+        beforeRouteEnter(to, from, next) {
+            next(() => {
+                injection.sidebar.active('mall');
+            });
+        },
+        data() {
+            return {};
+        },
+        methods: {},
+    };
+</script>
+<template>
+    <div class="mall-wrap">
+        <div class="home-wrap">
+            <tabs value="home">
+                <tab-pane label="首页" name="home">
+                    <card :bordered="false">
+
+                    </card>
+                </tab-pane>
+            </tabs>
+        </div>
+    </div>
+</template>
