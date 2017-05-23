fd5109adfac3dc8341338c860513a066ed47a1a4
twilroad <269044570@qq.com>
2017-05-23 19:48:07 +0800

    add router, entry, and handler for seller/product/create api

--- src/Controllers/Api/Seller/ProductController.php
+++ src/Controllers/Api/Seller/ProductController.php
@@ -9,6 +9,7 @@
 namespace Notadd\Mall\Controllers\Api\Seller;
 
 use Notadd\Foundation\Routing\Abstracts\Controller;
+use Notadd\Mall\Handlers\Seller\Product\CreateHandler;
 use Notadd\Mall\Handlers\Seller\Product\ListHandler;
 
 /**
@@ -16,6 +17,17 @@ use Notadd\Mall\Handlers\Seller\Product\ListHandler;
  */
 class ProductController extends Controller
 {
+    /**
+     * @param \Notadd\Mall\Handlers\Seller\Product\CreateHandler $handler
+     *
+     * @return \Notadd\Foundation\Passport\Responses\ApiResponse|\Psr\Http\Message\ResponseInterface|\Zend\Diactoros\Response
+     * @throws \Exception
+     */
+    public function create(CreateHandler $handler)
+    {
+        return $handler->toResponse()->generateHttpResponse();
+    }
+
     /**
      * @param \Notadd\Mall\Handlers\Seller\Product\ListHandler $handler
      *
---
+++ src/Handlers/Seller/Product/CreateHandler.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author        TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime      2017-05-23 19:47
+ */
+namespace Notadd\Mall\Handlers\Seller\Product;
+
+use Notadd\Foundation\Passport\Abstracts\SetHandler;
+
+/**
+ * Class CreateHandler.
+ */
+class CreateHandler extends SetHandler
+{
+}
