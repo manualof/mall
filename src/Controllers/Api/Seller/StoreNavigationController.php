5ba098f07146261d2d5628b1d939bae5ceaf6bc6
twilroad <269044570@qq.com>
2017-05-23 18:08:11 +0800

    add router, entry, and handler for seller/store/navigation api

--- src/Controllers/Api/Seller/StoreNavigationController.php
+++ src/Controllers/Api/Seller/StoreNavigationController.php
@@ -12,6 +12,7 @@ use Notadd\Foundation\Routing\Abstracts\Controller;
 use Notadd\Mall\Handlers\Seller\Store\Navigation\CreateHandler;
 use Notadd\Mall\Handlers\Seller\Store\Navigation\EditHandler;
 use Notadd\Mall\Handlers\Seller\Store\Navigation\ListHandler;
+use Notadd\Mall\Handlers\Seller\Store\Navigation\NavigationHandler;
 use Notadd\Mall\Handlers\Seller\Store\Navigation\RemoveHandler;
 
 /**
@@ -48,6 +49,17 @@ class StoreNavigationController extends Controller
      * @throws \Exception
      */
     public function list(ListHandler $handler)
+    {
+        return $handler->toResponse()->generateHttpResponse();
+    }
+
+    /**
+     * @param \Notadd\Mall\Handlers\Seller\Store\Navigation\NavigationHandler $handler
+     *
+     * @return \Notadd\Foundation\Passport\Responses\ApiResponse|\Psr\Http\Message\ResponseInterface|\Zend\Diactoros\Response
+     * @throws \Exception
+     */
+    public function navigation(NavigationHandler $handler)
     {
         return $handler->toResponse()->generateHttpResponse();
     }
