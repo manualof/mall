cfccffcda3b203d69f1a18f8180fec0358d56613
twilroad <269044570@qq.com>
2017-05-23 13:52:08 +0800

    add some controller classes

---
+++ src/Controllers/Api/Seller/OrderExpressController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime 2017-05-23 13:50
+ */
+namespace Notadd\Mall\Controllers\Api\Seller;
+
+use Notadd\Foundation\Routing\Abstracts\Controller;
+
+/**
+ * Class OrderExpressController.
+ */
+class OrderExpressController extends Controller
+{
+
+}
