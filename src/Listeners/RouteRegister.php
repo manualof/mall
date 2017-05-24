ec5be4d70911276c3e83af051dc19a11429d88c2
twilroad <269044570@qq.com>
2017-05-24 17:40:42 +0800

    add router, entry, and handler for user/footprint/list api

--- src/Listeners/RouteRegister.php
+++ src/Listeners/RouteRegister.php
@@ -274,6 +274,7 @@ class RouteRegister extends AbstractRouteRegister
             $this->router->post('follow/edit', FollowController::class . '@edit');
             $this->router->post('follow/list', FollowController::class . '@list');
             $this->router->post('follow/remove', FollowController::class . '@remove');
+            $this->router->post('footprint/list', UserOrderController::class . '@list');
             $this->router->post('order', UserOrderController::class . '@order');
             $this->router->post('order/cancel', UserOrderController::class . '@cancel');
             $this->router->post('order/edit', UserOrderController::class . '@edit');
