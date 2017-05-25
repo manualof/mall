e212a33db570d93318f1e8c2ee2589fb4cd1dcaa
twilroad <269044570@qq.com>
2017-05-25 12:39:05 +0800

    add entry controllers for foreground

---
+++ src/Controllers/MallController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author        TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime      2017-05-25 12:35
+ */
+namespace Notadd\Mall\Controllers;
+
+use Notadd\Foundation\Routing\Abstracts\Controller;
+
+/**
+ * Class MallController.
+ */
+class MallController extends Controller
+{
+}
---
+++ src/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author        TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime      2017-05-25 12:36
+ */
+namespace Notadd\Mall\Controllers;
+
+use Notadd\Foundation\Routing\Abstracts\Controller;
+
+/**
+ * Class UserController.
+ */
+class UserController extends Controller
+{
+}
