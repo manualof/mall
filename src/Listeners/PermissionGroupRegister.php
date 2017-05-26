a426ada7d0a8eda5ab9429804363e050815e2278
twilroad <269044570@qq.com>
2017-05-26 11:25:22 +0800

    add permission group register

---
+++ src/Listeners/PermissionGroupRegister.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime 2017-05-26 11:02
+ */
+namespace Notadd\Mall\Listeners;
+
+use Notadd\Foundation\Permission\Abstracts\PermissionGroupRegister as AbstractPermissionGroupRegister;
+
+/**
+ * Class PermissionGroupRegister.
+ */
+class PermissionGroupRegister extends AbstractPermissionGroupRegister
+{
+    /**
+     * Handle Permission Register.
+     */
+    public function handle()
+    {
+        $this->manager->group('mall', [
+            'description' => '商城权限定义。',
+            'identification' => 'global',
+            'name' => '商城权限',
+        ]);
+    }
+}
