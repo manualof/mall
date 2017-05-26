7e3a4abd6129f8b953226f8633c339175291ce68
TianJin <tfanxi@163.com>
2017-05-26 14:55:38 +0800

    add shop message module content

--- resources/mixes/seller/src/pages/ShopMessage.vue
+++ resources/mixes/seller/src/pages/ShopMessage.vue
@@ -1,5 +1,6 @@
 <script>
     import injection from '../helpers/injection';
+    import image from '../assets/images/img_logo.png';
 
     export default {
         beforeRouteEnter(to, from, next) {
@@ -8,7 +9,24 @@
             });
         },
         data() {
-            return {};
+            return {
+                shopMessage: {
+                    businessScope: '服饰',
+                    companyAddress: '高新二路220号国土资源大厦公寓楼',
+                    companyEmail: '336645564@qq.com',
+                    companyName: '本初网络',
+                    companyPhone: '029-2222365',
+                    companyPlace: '陕西省西安市雁塔区',
+                    contactName: 'ddd',
+                    contactPhone: '13626468989',
+                    employeesNum: '50',
+                    licenseNo: '3326568946464',
+                    licensePicture: image,
+                    licensePlace: '陕西省西安市雁塔区',
+                    licenseTime: '2010/10/0-2020/12/20',
+                    registered: '10w',
+                },
+            };
         },
         methods: {
         },
@@ -25,9 +43,99 @@
                                 <h5>商品基本信息</h5>
                                 <div>
                                     <row>
-                                        <i-col span="12">
-                                            <form-item label="商品名称">
-                                                rdygrt
+                                        <i-col  span="12">
+                                            <form-item label="公司名称">
+                                                {{ shopMessage.companyName }}
+                                            </form-item>
+                                        </i-col>
+                                    </row>
+                                    <row>
+                                        <i-col  span="12">
+                                            <form-item label="所在地">
+                                                {{ shopMessage.companyPlace }}
+                                            </form-item>
+                                        </i-col>
+                                    </row>
+                                    <row>
+                                        <i-col  span="12">
+                                            <form-item label="详细地址">
+                                                {{ shopMessage.companyAddress }}
+                                            </form-item>
+                                        </i-col>
+                                    </row>
+                                    <row>
+                                        <i-col  span="12">
+                                            <form-item label="公司电话">
+                                                {{ shopMessage.companyPhone }}
+                                            </form-item>
+                                        </i-col>
+                                        <i-col  span="12">
+                                            <form-item label="电子邮箱">
+                                                {{ shopMessage.companyEmail }}
+                                            </form-item>
+                                        </i-col>
+                                    </row>
+                                    <row>
+                                        <i-col  span="12">
+                                            <form-item label="员工总数">
+                                                {{ shopMessage.employeesNum }}人
+                                            </form-item>
+                                        </i-col>
+                                        <i-col  span="12">
+                                            <form-item label="注册资金">
+                                                {{ shopMessage.registered }}
+                                            </form-item>
+                                        </i-col>
+                                    </row>
+                                    <row>
+                                        <i-col  span="12">
+                                            <form-item label="联系人姓名">
+                                                {{ shopMessage.contactName }}
+                                            </form-item>
+                                        </i-col>
+                                        <i-col  span="12">
+                                            <form-item label="联系人电话">
+                                                {{ shopMessage.contactPhone }}
+                                            </form-item>
+                                        </i-col>
+                                    </row>
+                                </div>
+                            </div>
+                            <div class="library-application">
+                                <h5>营业执照信息（副本）</h5>
+                                <div>
+                                    <row>
+                                        <i-col  span="12">
+                                            <form-item label="营业执照号">
+                                                {{ shopMessage.companyName }}
+                                            </form-item>
+                                        </i-col>
+                                    </row>
+                                    <row>
+                                        <i-col  span="12">
+                                            <form-item label="营业执照所在地">
+                                                {{ shopMessage.companyPlace }}
+                                            </form-item>
+                                        </i-col>
+                                    </row>
+                                    <row>
+                                        <i-col  span="12">
+                                            <form-item label="营业执照有效期">
+                                                {{ shopMessage.companyAddress }}
+                                            </form-item>
+                                        </i-col>
+                                    </row>
+                                    <row>
+                                        <i-col  span="12">
+                                            <form-item label="法定经营范围">
+                                                {{ shopMessage.companyPhone }}
+                                            </form-item>
+                                        </i-col>
+                                    </row>
+                                    <row>
+                                        <i-col  span="12">
+                                            <form-item label="营业执照电子版">
+                                                {{ shopMessage.companyPhone }}
                                             </form-item>
                                         </i-col>
                                     </row>
