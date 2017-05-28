7282cf427fbe07458dde8ce840c3e0a0bb15199c
twilroad <269044570@qq.com>
2017-05-28 20:34:54 +0800

    add base logic for product category handler

--- src/Handlers/Admin/Product/Category/EditHandler.php
+++ src/Handlers/Admin/Product/Category/EditHandler.php
@@ -9,6 +9,7 @@
 namespace Notadd\Mall\Handlers\Admin\Product\Category;
 
 use Notadd\Foundation\Passport\Abstracts\Handler;
+use Notadd\Mall\Models\ProductCategory;
 
 /**
  * Class EditHandler.
@@ -22,6 +23,12 @@ class EditHandler extends Handler
      */
     public function execute()
     {
-        // TODO: Implement execute() method.
+        $id = $this->request->input('id');
+        $category = ProductCategory::query()->find($id);
+        if ($category && $category->update($this->request->all())) {
+            $this->withCode(200)->withMessage('');
+        } else {
+            $this->withCode(500)->withError('');
+        }
     }
 }
--- src/Handlers/Admin/Product/Category/RemoveHandler.php
+++ src/Handlers/Admin/Product/Category/RemoveHandler.php
@@ -9,6 +9,7 @@
 namespace Notadd\Mall\Handlers\Admin\Product\Category;
 
 use Notadd\Foundation\Passport\Abstracts\Handler;
+use Notadd\Mall\Models\ProductCategory;
 
 /**
  * Class RemoveHandler.
@@ -22,6 +23,12 @@ class RemoveHandler extends Handler
      */
     public function execute()
     {
-        // TODO: Implement execute() method.
+        $id = $this->request->input('id');
+        $category = ProductCategory::query()->find($id);
+        if ($category && $category->delete()) {
+            $this->withCode(200)->withMessage('');
+        } else {
+            $this->withCode(500)->withError('');
+        }
     }
 }
