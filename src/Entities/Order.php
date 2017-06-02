e97fdc4b9ab752cfc3e043f3f78e1934e3c007f6
twilroad <269044570@qq.com>
2017-06-02 16:53:25 +0800

    extend more places and transitions for order flow

--- src/Entities/Order.php
+++ src/Entities/Order.php
@@ -16,79 +16,84 @@ use Symfony\Component\Workflow\Transition;
  */
 class Order extends Entity
 {
+    public function announceCancel()
+    {
+    }
+
     public function announceDeliver()
     {
-
     }
 
     public function announcePay()
     {
-
     }
 
     public function announceTake()
     {
+    }
 
+    public function enterCancel()
+    {
+    }
+
+    public function enterCancelled()
+    {
     }
 
     public function enterDeliver()
     {
-
     }
 
     public function enterDelivered()
     {
-
     }
 
     public function enterPay()
     {
-
     }
 
     public function enterPayed()
     {
-
     }
 
     public function enterTake()
     {
-
     }
 
     public function enterTook()
     {
+    }
 
+    public function enteredCancel()
+    {
+    }
+
+    public function enteredCancelled()
+    {
     }
 
     public function enteredDeliver()
     {
-
     }
 
     public function enteredDelivered()
     {
-
     }
 
     public function enteredPay()
     {
-
     }
 
     public function enteredPayed()
     {
-
     }
 
     public function enteredTake()
     {
-
     }
 
     public function enteredTook()
     {
-
     }
 
     /**
@@ -97,15 +102,20 @@ class Order extends Entity
     public function events()
     {
         return [
+            'flow.mall.order.announce.cancel'    => 'announceCancel',
             'flow.mall.order.announce.deliver'   => 'announceDeliver',
             'flow.mall.order.announce.pay'       => 'announcePay',
             'flow.mall.order.announce.take'      => 'announceTake',
+            'flow.mall.order.enter.cancel'       => 'enterCancel',
+            'flow.mall.order.enter.cancelled'    => 'enterCancelled',
             'flow.mall.order.enter.deliver'      => 'enterDeliver',
             'flow.mall.order.enter.delivered'    => 'enterDelivered',
             'flow.mall.order.enter.pay'          => 'enterPay',
             'flow.mall.order.enter.payed'        => 'enterPayed',
             'flow.mall.order.enter.take'         => 'enterTake',
             'flow.mall.order.enter.took'         => 'enterTook',
+            'flow.mall.order.entered.cancel'     => 'enteredCancel',
+            'flow.mall.order.entered.cancelled'  => 'enteredCancelled',
             'flow.mall.order.entered.deliver'    => 'enteredDeliver',
             'flow.mall.order.entered.delivered'  => 'enteredDelivered',
             'flow.mall.order.entered.pay'        => 'enteredPay',
@@ -113,14 +123,18 @@ class Order extends Entity
             'flow.mall.order.entered.take'       => 'enteredTake',
             'flow.mall.order.entered.took'       => 'enteredTook',
             'flow.mall.order.guard.deliver'      => 'guardDeliver',
+            'flow.mall.order.guard.cancel'       => 'guardCancel',
             'flow.mall.order.guard.pay'          => 'guardPay',
             'flow.mall.order.guard.take'         => 'guardTake',
+            'flow.mall.order.leave.cancel'       => 'leaveCancel',
+            'flow.mall.order.leave.cancelled'    => 'leaveCancelled',
             'flow.mall.order.leave.deliver'      => 'leaveDeliver',
             'flow.mall.order.leave.delivered'    => 'leaveDelivered',
             'flow.mall.order.leave.pay'          => 'leavePay',
             'flow.mall.order.leave.payed'        => 'leavePayed',
             'flow.mall.order.leave.take'         => 'leaveTake',
             'flow.mall.order.leave.took'         => 'leaveTook',
+            'flow.mall.order.transition.cancel'  => 'transitionCancel',
             'flow.mall.order.transition.deliver' => 'transitionDeliver',
             'flow.mall.order.transition.pay'     => 'transitionPay',
             'flow.mall.order.transition.take'    => 'transitionTake',
@@ -129,47 +143,46 @@ class Order extends Entity
 
     public function guardDeliver()
     {
-
     }
 
     public function guardPay()
     {
-
     }
 
     public function guardTake()
     {
+    }
 
+    public function leaveCancel()
+    {
+    }
+
+    public function leaveCancelled()
+    {
     }
 
     public function leaveDeliver()
     {
-
     }
 
     public function leaveDelivered()
     {
-
     }
 
     public function leavePay()
     {
-
     }
 
     public function leavePayed()
     {
-
     }
 
     public function leaveTake()
     {
-
     }
 
     public function leaveTook()
     {
-
     }
 
     /**
@@ -186,6 +199,8 @@ class Order extends Entity
     public function places()
     {
         return [
+            'cancel',    // 取消
+            'cancelled', // 已取消
             'pay',       // 等待支付
             'payed',     // 支付完成
             'deliver',   // 等待发货
@@ -195,14 +210,16 @@ class Order extends Entity
         ];
     }
 
+    public function transitionCancel()
+    {
+    }
+
     public function transitionDeliver()
     {
-
     }
 
     public function transitionPay()
     {
-
     }
 
     /**
@@ -211,14 +228,14 @@ class Order extends Entity
     public function transitions()
     {
         return [
-            new Transition('pay', 'pay', 'payed'),
+            new Transition('cancel', 'cancel', 'cancelled'),
             new Transition('deliver', 'deliver', 'delivered'),
+            new Transition('pay', 'pay', 'payed'),
             new Transition('take', 'take', 'took'),
         ];
     }
 
     public function transitionTake()
     {
-
     }
 }
