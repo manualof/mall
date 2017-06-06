c974aff93b643446b92ba24173d98b1dee74d52d
eyvv <1009749309@qq.com>
2017-06-06 15:47:18 +0800

    售后服务配置

---
+++ resources/mixes/seller/src/pages/ServiceRefund.vue
@@ -0,0 +1,23 @@
+<script>
+    import injection from '../helpers/injection';
+
+    export default {
+        beforeRouteEnter(to, from, next) {
+            next(() => {
+                injection.sidebar.active('seller');
+            });
+        },
+        data() {
+            return {
+                msg: 'Welcome to Your Vue.js App',
+            };
+        },
+    };
+</script>
+<template>
+    <div class="seller-wrap">
+        <div class="service-refund">
+            fgfgjk
+        </div>
+    </div>
+</template>
