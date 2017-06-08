254130d83bc6020c8b47002541b8472305549668
TianJin <tfanxi@163.com>
2017-06-08 20:17:54 +0800

    add order waybill self template module content

--- resources/mixes/seller/src/pages/OrderWaybill.vue
+++ resources/mixes/seller/src/pages/OrderWaybill.vue
@@ -11,6 +11,93 @@
         data() {
             return {
                 self: this,
+                selfTemplateColumns: [
+                    {
+                        align: 'center',
+                        key: 'company',
+                        title: '物流公司',
+                        width: 120,
+                    },
+                    {
+                        align: 'center',
+                        key: 'template',
+                        title: '运单模版',
+                        width: 120,
+                    },
+                    {
+                        align: 'center',
+                        key: 'example',
+                        render() {
+                            return `<div class="example-module">
+                                        <img :src="row.img" alt="">
+                                        <div>
+                                            <p>宽度: {{ row.width }}</p>
+                                            <p>高度: {{ row.height }}</p>
+                                        </div>
+                                    </div>`;
+                        },
+                        title: '运单图例',
+                    },
+                    {
+                        align: 'center',
+                        key: 'topWidth',
+                        title: '上偏移量',
+                        width: 120,
+                    },
+                    {
+                        align: 'center',
+                        key: 'leftWidth',
+                        title: '左偏移量',
+                        width: 120,
+                    },
+                    {
+                        align: 'center',
+                        key: 'default',
+                        render() {
+                            return `<i-switch size="large" v-model="row.status">
+                                        <span slot="open">开启</span>
+                                        <span slot="close">关闭</span>
+                                    </i-switch>`;
+                        },
+                        title: '启用',
+                        width: 120,
+                    },
+                    {
+                        align: 'center',
+                        key: 'action',
+                        render() {
+                            return `<i-button class="action-btn" size="small" type="ghost">设计</i-button>
+                                    <i-button class="action-btn" size="small" type="ghost">测试</i-button>
+                                    <i-button class="action-btn" size="small" type="ghost">编辑</i-button>
+                                    <i-button class="action-btn" size="small" type="ghost">删除</i-button>`;
+                        },
+                        title: '操作',
+                    },
+                ],
+                selfTemplateData: [
+                    {
+                        company: '顺丰速运',
+                        height: '139mm',
+                        img: image,
+                        isTemplate: true,
+                        leftWidth: 2,
+                        status: true,
+                        template: '常用模版1',
+                        topWidth: 2,
+                        width: '216mm',
+                    },
+                    {
+                        company: '顺丰速运',
+                        height: '139mm',
+                        img: image,
+                        isTemplate: true,
+                        leftWidth: 2,
+                        status: true,
+                        template: '常用模版1',
+                        topWidth: 2,
+                        width: '216mm',
+                    },
+                ],
                 templateColumns: [
                     {
                         align: 'center',
@@ -47,6 +134,19 @@
                                     </i-switch>`;
                         },
                         title: '默认',
+                    },
+                    {
+                        align: 'center',
+                        key: 'action',
+                        render() {
+                            return `<div v-if="row.isTemplate">
+                                        <i-button class="edit-btn" size="small" type="ghost">编辑</i-button>
+                                        <i-button size="small" type="ghost">解绑</i-button>
+                                    </div>
+                                    <i-button size="small" type="ghost" v-if="!row.isTemplate">选择模板</i-button>`;
+                        },
+                        title: '操作',
+                        width: 160,
                     },
                 ],
                 templateData: [
@@ -96,7 +196,17 @@
                 </tab-pane>
                 <tab-pane label="自建模版" name="name2">
                     <card :bordered="false">
-
+                        <div class="prompt-box">
+                            <p>提示</p>
+                            <p>1.未绑定的物流公司后边会出现“选择模板”按钮，在选择模板页面可以绑定可用的打印模板</p>
+                            <p>2.点击“设置”按钮可以设置自定义的内容，包括偏移量和需要显示的项目</p>
+                            <p>3.点击“默认”按钮可以设置当前模板为默认打印模板</p>
+                            <p>4.点击“解绑”按钮可以解除当前绑定，重新选择其他模板</p>
+                        </div>
+                        <i-button class="first-btn" type="ghost">+添加模板</i-button>
+                        <i-table :columns="selfTemplateColumns"
+                                 :context="self"
+                                 :data="selfTemplateData"></i-table>
                     </card>
                 </tab-pane>
             </tabs>
