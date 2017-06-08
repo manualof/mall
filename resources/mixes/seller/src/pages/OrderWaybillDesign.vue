a888c803ef955f26c3fc9163091bcf7060918b7b
TianJin <tfanxi@163.com>
2017-06-08 21:33:36 +0800

    add template design adjust modal

--- resources/mixes/seller/src/pages/OrderWaybillDesign.vue
+++ resources/mixes/seller/src/pages/OrderWaybillDesign.vue
@@ -9,8 +9,15 @@
         },
         data() {
             return {
+                adjust: false,
                 designForm: {
                     select: [],
+                },
+                editForm: {
+                    bottom: '',
+                    left: '',
+                    height: '',
+                    width: '',
                 },
                 loading: false,
                 selectList: [
@@ -39,6 +46,9 @@
             };
         },
         methods: {
+            edit() {
+                this.adjust = true;
+            },
             goBack() {
                 const self = this;
                 self.$router.go(-1);
@@ -47,6 +57,20 @@
                 const self = this;
                 self.loading = true;
                 self.$refs.designForm.validate(valid => {
+                    if (valid) {
+                        window.console.log(valid);
+                    } else {
+                        self.loading = false;
+                        self.$notice.error({
+                            title: '请正确填写设置信息！',
+                        });
+                    }
+                });
+            },
+            submitEdit() {
+                const self = this;
+                self.loading = true;
+                self.$refs.editForm.validate(valid => {
                     if (valid) {
                         window.console.log(valid);
                     } else {
@@ -77,7 +101,7 @@
                                 <checkbox-group v-model="designForm.select">
                                     <checkbox :label="item.name" v-for="item in selectList">
                                         <span>{{ item.name }}</span>
-                                        <icon type="edit"></icon>
+                                        <icon type="edit" @click.native="edit(item)"></icon>
                                     </checkbox>
                                 </checkbox-group>
                             </form-item>
@@ -103,6 +127,56 @@
                         </i-col>
                     </row>
                 </i-form>
+                <modal
+                        v-model="adjust"
+                        title="微调" class="upload-picture-modal adjust-template-design">
+                    <div>
+                        <i-form ref="editForm" :model="editForm" :rules="editValidate" :label-width="100">
+                            <form-item label="左偏移量">
+                                <row>
+                                    <i-col span="4">
+                                        <i-input v-model="editForm.left"></i-input>
+                                    </i-col>
+                                    <i-col span="2">mm</i-col>
+                                </row>
+                            </form-item>
+                            <form-item label="下偏移量">
+                                <row>
+                                    <i-col span="4">
+                                        <i-input v-model="editForm.bottom"></i-input>
+                                    </i-col>
+                                    <i-col span="2">mm</i-col>
+                                </row>
+                            </form-item>
+                            <form-item label="宽">
+                                <row>
+                                    <i-col span="4">
+                                        <i-input v-model="editForm.width"></i-input>
+                                    </i-col>
+                                    <i-col span="2">mm</i-col>
+                                </row>
+                            </form-item>
+                            <form-item label="高">
+                                <row>
+                                    <i-col span="4">
+                                        <i-input v-model="editForm.height"></i-input>
+                                    </i-col>
+                                    <i-col span="2">mm</i-col>
+                                </row>
+                            </form-item>
+                            <row>
+                                <i-col span="20">
+                                    <form-item>
+                                        <i-button :loading="loading" type="primary" @click.native="submitEdit">
+                                            <span v-if="!loading">确认提交</span>
+                                            <span v-else>正在提交…</span>
+                                        </i-button>
+                                    </form-item>
+                                </i-col>
+                            </row>
+                        </i-form>
+                    </div>
+                </modal>
             </card>
         </div>
     </div>
