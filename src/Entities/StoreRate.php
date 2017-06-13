42528c0ea92054c7531bd7e62ea700c9520ba37b
twilroad <269044570@qq.com>
2017-06-13 18:25:30 +0800

    add guard logic for store rate flow

--- src/Entities/StoreRate.php
+++ src/Entities/StoreRate.php
@@ -81,7 +81,19 @@ class StoreRate extends Entity
      */
     public function guard(GuardEvent $event)
     {
-        // TODO: Implement guard() method.
+        switch ($event->getTransition()->getName()) {
+            case 'rate':
+                $this->block($event, $this->permission(''));
+                break;
+            case 'wait_to_review':
+                $this->block($event, $this->permission(''));
+                break;
+            case 'review':
+                $this->block($event, $this->permission(''));
+                break;
+            default:
+                $event->setBlocked(true);
+        }
     }
 
     /**
