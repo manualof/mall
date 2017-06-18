c319d704e8beaf1a1778e8739d465f81742653df
twilroad <269044570@qq.com>
2017-06-18 21:37:47 +0800

    fix style for codes

--- resources/mixes/store/.eslintrc.js
+++ resources/mixes/store/.eslintrc.js
@@ -1,39 +1,49 @@
-// http://eslint.org/docs/user-guide/configuring
-
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  env: {
-    browser: true,
-  },
-  extends: 'airbnb-base',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // check if imports actually resolve
-  'settings': {
-    'import/resolver': {
-      'webpack': {
-        'config': 'build/webpack.base.conf.js'
-      }
-    }
-  },
-  // add your custom rules here
-  'rules': {
-    // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      'js': 'never',
-      'vue': 'never'
-    }],
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      'optionalDependencies': ['test/unit/index.js']
-    }],
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
-}
+    root: true,
+    parser: 'babel-eslint',
+    parserOptions: {
+        sourceType: 'module',
+    },
+    env: {
+        browser: true,
+    },
+    extends: 'airbnb-base',
+    // required to lint *.vue files
+    plugins: [
+        'html',
+    ],
+    // check if imports actually resolve
+    settings: {
+        'import/resolver': {
+            webpack: {
+                config: 'build/webpack.base.conf.js',
+            },
+        },
+    },
+    rules: {
+        'arrow-parens': [2, 'as-needed', {
+            requireForBlockBody: false,
+        }],
+        'eol-last': 0,
+        'guard-for-in': 0,
+        indent: ['error', 4, {
+            SwitchCase: 1,
+        }],
+        'import/extensions': ['error', 'always', {
+            js: 'never',
+            vue: 'never',
+        }],
+        'import/no-extraneous-dependencies': ['error', {
+            optionalDependencies: ['test/unit/index.js'],
+        }],
+        'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+        'no-param-reassign': ['error', {
+            props: false,
+        }],
+        'no-plusplus': ['error', {
+            allowForLoopAfterthoughts: true,
+        }],
+        'no-restricted-syntax': ['error', 'WithStatement', "BinaryExpression[operator='in']"],
+    },
+};
