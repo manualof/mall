4e79ecb9882e1a947626d00f00717353c9c38790
twilroad <269044570@qq.com>
2017-06-21 17:42:29 +0800

    enhance api admin/product/category

--- src/Handlers/Admin/Product/Category/RemoveHandler.php
+++ src/Handlers/Admin/Product/Category/RemoveHandler.php
@@ -23,12 +23,20 @@ class RemoveHandler extends Handler
      */
     public function execute()
     {
-        $id = $this->request->input('id');
-        $category = ProductCategory::query()->find($id);
-        if ($category && $category->delete()) {
-            $this->withCode(200)->withMessage('');
+        $this->validate($this->request, [
+            'id' => 'required|numeric',
+        ], [
+            'id.numeric'  => '分类 ID 必须为数值',
+            'id.required' => '分类 ID 必须填写',
+        ]);
+        $this->beginTransaction();
+        $category = ProductCategory::query()->find($this->request->input('id'));
+        if ($category instanceof ProductCategory && $category->delete()) {
+            $this->commitTransaction();
+            $this->withCode(200)->withMessage('移除分类成功！');
         } else {
-            $this->withCode(500)->withError('');
+            $this->rollBackTransaction();
+            $this->withCode(500)->withError('移除分类失败！');
         }
     }
 }
