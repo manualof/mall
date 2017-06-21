510a4e2e0fa6f75f1ae0e099a47fc54532c6194d
twilroad <269044570@qq.com>
2017-06-21 20:30:50 +0800

    add logic for api admin/specification/create

--- src/Handlers/Admin/Product/Specification/CreateHandler.php
+++ src/Handlers/Admin/Product/Specification/CreateHandler.php
@@ -23,10 +23,31 @@ class CreateHandler extends Handler
      */
     public function execute()
     {
-        if (ProductSpecification::query()->create($this->request->all())) {
-            $this->withCode(200)->withMessage('');
+        $this->validate($this->request, [
+            'category_id' => 'required|numeric',
+            'name'        => 'required',
+            'type'        => 'required',
+            'value'       => 'required',
+        ], [
+            'category_id.numeric'  => '分类 ID 必须为数值',
+            'category_id.required' => '分类 ID 必须填写',
+            'name.required'        => '规格显示名称必须填写',
+            'type.required'        => '规格类型必须填写',
+            'value.required'       => '规格值必须填写',
+        ]);
+        $this->beginTransaction();
+        $data = $this->request->only([
+            'category_id',
+            'name',
+            'type',
+            'value',
+        ]);
+        if (ProductSpecification::query()->create($data)) {
+            $this->commitTransaction();
+            $this->withCode(200)->withMessage('添加产品规格成功！');
         } else {
-            $this->withCode(500)->withError('');
+            $this->rollBackTransaction();
+            $this->withCode(500)->withError('添加产品规格失败！');
         }
     }
 }
