550c86aac89db7fa87f78ddf4cc0883f6889967b
twilroad <269044570@qq.com>
2017-06-21 20:38:37 +0800

    add logic for api admin/specification

--- src/Handlers/Admin/Product/Specification/SpecificationHandler.php
+++ src/Handlers/Admin/Product/Specification/SpecificationHandler.php
@@ -9,6 +9,7 @@
 namespace Notadd\Mall\Handlers\Admin\Product\Specification;
 
 use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Mall\Models\ProductSpecification;
 
 /**
  * Class SpecificationHandler.
@@ -22,6 +23,17 @@ class SpecificationHandler extends Handler
      */
     protected function execute()
     {
-        // TODO: Implement execute() method.
+        $this->validate($this->request, [
+            'id' => 'required|numeric',
+        ], [
+            'id.numeric'  => '规格 ID 必须为数值',
+            'id.required' => '规格 ID 必须填写',
+        ]);
+        $specification = ProductSpecification::query()->find($this->request->input('id'));
+        if ($specification instanceof ProductSpecification) {
+            $this->withCode(200)->withData($specification)->withMessage('获取产品规格数据成功！');
+        } else {
+            $this->withCode(500)->withError('获取产品规格数据失败！');
+        }
     }
 }
