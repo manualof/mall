9efc3a4b42117c6df01f519bd5653ebafe6dced8
twilroad <269044570@qq.com>
2017-06-21 15:59:53 +0800

    add route, entry, and handler for api product/brand/create

---
+++ src/Controllers/Api/Admin/ProductBrandController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime 2017-06-21 15:53
+ */
+namespace Notadd\Mall\Controllers\Api\Admin;
+
+use Notadd\Foundation\Routing\Abstracts\Controller;
+
+/**
+ * Class ProductBrandController.
+ */
+class ProductBrandController extends Controller
+{
+    public function create()
+    {
+        
+    }
+}
