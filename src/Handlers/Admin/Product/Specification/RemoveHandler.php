da9adcd6130e1bb9f8c1bf5a239d68439ca72397
twilroad <269044570@qq.com>
2017-06-21 20:32:47 +0800

    add logic for api admin/specification/remove

--- src/Handlers/Admin/Product/Specification/RemoveHandler.php
+++ src/Handlers/Admin/Product/Specification/RemoveHandler.php
@@ -23,12 +23,20 @@ class RemoveHandler extends Handler
      */
     public function execute()
     {
-        $id = $this->request->input('id');
-        $specification = ProductSpecification::query()->find($id);
-        if ($specification && $specification->delete()) {
-            $this->withCode(200)->withMessage('');
+        $this->validate($this->request, [
+            'id' => 'required|numeric',
+        ], [
+            'id.numeric'  => '规格 ID 必须为数值',
+            'id.required' => '规格 ID 必须填写',
+        ]);
+        $this->beginTransaction();
+        $specification = ProductSpecification::query()->find($this->request->input('id'));
+        if ($specification instanceof ProductSpecification && $specification->delete()) {
+            $this->commitTransaction();
+            $this->withCode(200)->withMessage('删除产品规格成功！');
         } else {
-            $this->withCode(500)->withError('');
+            $this->rollBackTransaction();
+            $this->withCode(500)->withError('删除产品规格失败！');
         }
     }
 }
