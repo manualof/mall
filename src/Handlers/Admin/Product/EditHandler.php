939fc87761da6696fccf861c0cb5482b74ef36ae
twilroad <269044570@qq.com>
2017-06-21 20:23:21 +0800

    add logic for api admin/product/edit

--- src/Handlers/Admin/Product/EditHandler.php
+++ src/Handlers/Admin/Product/EditHandler.php
@@ -23,12 +23,56 @@ class EditHandler extends Handler
      */
     public function execute()
     {
-        $id = $this->request->input('id');
-        $product = Product::query()->find($id);
-        if ($product && $product->update($this->request->all())) {
-            $this->withCode(200)->withMessage('');
+        $this->validate($this->request, [
+            'barcode'           => 'numeric',
+            'brand_id'          => 'numeric',
+            'business_item'     => 'numeric',
+            'category_id'       => 'numeric',
+            'description'       => 'required',
+            'id'                => 'required|numeric',
+            'name'              => 'required',
+            'price'             => 'required|numeric',
+            'price_cost'        => 'required|numeric',
+            'price_market'      => 'numeric',
+            'inventory'         => 'required|numeric',
+            'inventory_warning' => 'numeric',
+        ], [
+            'barcode.numeric'           => '商品条形码必须为数值',
+            'brand_id.numeric'          => '品牌 ID 必须为数值',
+            'business_item.numeric'     => '商家货号必须为数值',
+            'category_id.numeric'       => '分类 ID 必须为数值',
+            'id.required'               => '商品 ID 必须填写',
+            'id.numeric'                => '商品 ID 必须为数值',
+            'name.required'             => '商品名称必须填写',
+            'price.numeric'             => '价格必须为数值',
+            'price.required'            => '价格必须填写',
+            'price_cost.numeric'        => '成本价格必须为数值',
+            'price_cost.required'       => '成本价格必须填写',
+            'price_market.numeric'      => '市场价格必须为数值',
+            'inventory.numeric'         => '库存必须为数值',
+            'inventory_warning.numeric' => '库存预警值必须为数值',
+        ]);
+        $this->beginTransaction();
+        $data = $this->request->only([
+            'barcode',
+            'brand_id',
+            'business_item',
+            'category_id',
+            'description',
+            'name',
+            'price',
+            'price_cost',
+            'price_market',
+            'inventory',
+            'inventory_warning',
+        ]);
+        $product = Product::query()->find($this->request->input('id'));
+        if ($product instanceof Product && $product->update($data)) {
+            $this->commitTransaction();
+            $this->withCode(200)->withMessage('修改产品信息成功！');
         } else {
-            $this->withCode(500)->withError('');
+            $this->rollBackTransaction();
+            $this->withCode(500)->withError('修改产品信息失败！');
         }
     }
 }
