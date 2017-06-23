c05797304d36019336e670694a182941da05de68
twilroad <269044570@qq.com>
2017-06-23 15:19:20 +0800

    add logic for product specification handler for admin and seller

--- src/Handlers/Admin/Product/ListHandler.php
+++ src/Handlers/Admin/Product/ListHandler.php
@@ -32,17 +32,20 @@ class ListHandler extends Handler
         ]);
         $builder = Product::query();
         $builder = $builder->paginate($this->request->input('paginate', 20));
-        $this->withCode(200)->withData($builder->items())->withMessage('获取产品列表成功！')->withExtra([
-            'pagination' => [
-                'total'         => $builder->total(),
-                'per_page'      => $builder->perPage(),
-                'current_page'  => $builder->currentPage(),
-                'last_page'     => $builder->lastPage(),
-                'next_page_url' => $builder->nextPageUrl(),
-                'prev_page_url' => $builder->previousPageUrl(),
-                'from'          => $builder->firstItem(),
-                'to'            => $builder->lastItem(),
-            ],
-        ]);
+        $this->withCode(200)
+            ->withData($builder->items())
+            ->withMessage('获取产品列表成功！')
+            ->withExtra([
+                'pagination' => [
+                    'total'         => $builder->total(),
+                    'per_page'      => $builder->perPage(),
+                    'current_page'  => $builder->currentPage(),
+                    'last_page'     => $builder->lastPage(),
+                    'next_page_url' => $builder->nextPageUrl(),
+                    'prev_page_url' => $builder->previousPageUrl(),
+                    'from'          => $builder->firstItem(),
+                    'to'            => $builder->lastItem(),
+                ],
+            ]);
     }
 }
