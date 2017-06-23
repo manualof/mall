69359b3ef92343db9f8284b887756cc3ad884b46
twilroad <269044570@qq.com>
2017-06-23 17:23:09 +0800

    add logic for store category remove handler for admin and seller

--- src/Handlers/Seller/Store/Category/RemoveHandler.php
+++ src/Handlers/Seller/Store/Category/RemoveHandler.php
@@ -9,6 +9,7 @@
 namespace Notadd\Mall\Handlers\Seller\Store\Category;
 
 use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Mall\Models\StoreCategory;
 
 /**
  * Class RemoveHandler.
@@ -22,6 +23,20 @@ class RemoveHandler extends Handler
      */
     public function execute()
     {
-        // TODO: Implement execute() method.
+        $this->validate($this->request, [
+            'id' => 'required|numeric',
+        ], [
+            'id.numeric'  => '分类 ID 必须为数值',
+            'id.required' => '分类 ID 必须填写',
+        ]);
+        $this->beginTransaction();
+        $category = StoreCategory::query()->find($this->request->input('id'));
+        if ($category instanceof StoreCategory && $category->delete()) {
+            $this->commitTransaction();
+            $this->withCode(200)->withMessage('删除店铺分类成功！');
+        } else {
+            $this->rollBackTransaction();
+            $this->withCode(500)->withError('删除店铺分类失败！');
+        }
     }
 }
