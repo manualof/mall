17f07c1dc124b432d9bdd0dd70dc9c90db97c185
twilroad <269044570@qq.com>
2017-06-23 17:08:28 +0800

    refactor fields for table mall_store_categories

--- databases/migrations/2017_04_24_166500_create_mall_store_categories_table.php
+++ databases/migrations/2017_04_24_166500_create_mall_store_categories_table.php
@@ -4,7 +4,6 @@
  *
  * @datetime 2017-05-09 13:54:48
  */
-
 use Illuminate\Database\Schema\Blueprint;
 use Notadd\Foundation\Database\Migrations\Migration;
 
@@ -22,10 +21,11 @@ class CreateMallStoreCategoriesTable extends Migration
     {
         $this->schema->create('mall_store_categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('amount_of_deposit')->default(0);
-            $table->tinyInteger('order')->default(0);
-            $table->integer('parent_id')->default(0);
-            $table->string('name');
+            $table->tinyInteger('order')->default(0)->comment('排序 ID');
+            $table->integer('parent_id')->default(0)->comment('父级 ID');
+            $table->string('name')->comment('分类名称');
+            $table->tinyInteger('status')->default(0)->comment('状态');
+            $table->integer('store_id')->comment('店铺 ID');
             $table->string('flow_marketing')->nullable();
             $table->timestamps();
             $table->softDeletes();
