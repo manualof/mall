dcd893023cb32c25743edd2526399f4adebbf851
twilroad <269044570@qq.com>
2017-06-23 15:48:25 +0800

    add logic for order list handler for admin and seller

--- src/Handlers/Admin/Order/ListHandler.php
+++ src/Handlers/Admin/Order/ListHandler.php
@@ -23,7 +23,29 @@ class ListHandler extends Handler
      */
     protected function execute()
     {
+        $this->validate($this->request, [
+            'page'     => 'numeric',
+            'paginate' => 'numeric',
+        ], [
+            'page.numeric'     => '当前页面必须为数值',
+            'paginate.numeric' => '分页数必须为数值',
+        ]);
         $builder = Order::query();
-        $this->withCode(200)->withData($builder->get())->withMessage('');
+        $builder = $builder->paginate($this->request->input('paginate', 20));
+        $this->withCode(200)
+            ->withData($builder->items())
+            ->withMessage('获取产品列表成功！')
+            ->withExtra([
+                'pagination' => [
+                    'total'         => $builder->total(),
+                    'per_page'      => $builder->perPage(),
+                    'current_page'  => $builder->currentPage(),
+                    'last_page'     => $builder->lastPage(),
+                    'next_page_url' => $builder->nextPageUrl(),
+                    'prev_page_url' => $builder->previousPageUrl(),
+                    'from'          => $builder->firstItem(),
+                    'to'            => $builder->lastItem(),
+                ],
+            ]);
     }
 }
