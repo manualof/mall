f5e92b55fe6c2296f1b0b28291e882d054b1db9d
twilroad <269044570@qq.com>
2017-06-23 17:30:51 +0800

    add logic for store dynamic remove handler for admin

--- src/Handlers/Admin/Store/Dynamic/RemoveHandler.php
+++ src/Handlers/Admin/Store/Dynamic/RemoveHandler.php
@@ -23,12 +23,20 @@ class RemoveHandler extends Handler
      */
     public function execute()
     {
-        $id = $this->request->input('id');
-        $dynamic = StoreDynamic::query()->find($id);
-        if ($dynamic && $dynamic->delete()) {
-            $this->withCode(200)->withMessage('');
+        $this->validate($this->request, [
+            'id' => 'required|numeric',
+        ], [
+            'id.numeric'  => '动态 ID 必须为数值',
+            'id.required' => '动态 ID 必须填写',
+        ]);
+        $this->beginTransaction();
+        $dynamic = StoreDynamic::query()->find($this->request->input('id'));
+        if ($dynamic instanceof StoreDynamic && $dynamic->delete()) {
+            $this->commitTransaction();
+            $this->withCode(200)->withMessage('删除店铺动态成功！');
         } else {
-            $this->withCode(500)->withError('');
+            $this->rollBackTransaction();
+            $this->withCode(500)->withError('删除店铺动态失败！');
         }
     }
 }
