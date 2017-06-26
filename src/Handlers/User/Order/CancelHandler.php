20cdcc11f9ac47227abc970d9560e46a7d602586
twilroad <269044570@qq.com>
2017-06-26 17:13:54 +0800

    add logic for user/order/cancel

--- src/Handlers/User/Order/CancelHandler.php
+++ src/Handlers/User/Order/CancelHandler.php
@@ -9,6 +9,7 @@
 namespace Notadd\Mall\Handlers\User\Order;
 
 use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Mall\Models\Order;
 
 /**
  * Class CancelHandler.
@@ -22,6 +23,21 @@ class CancelHandler extends Handler
      */
     public function execute()
     {
-        // TODO: Implement execute() method.
+        $this->validate($this->request, [
+            'id' => 'required|numeric',
+        ], [
+            'id.required' => '订单 ID 必须填写',
+            'id.numeric'  => '订单 ID 必须为数值',
+        ]);
+        $this->beginTransaction();
+        $order = Order::query()->find($this->request->input('id'));
+        $data = $this->request->only([]);
+        if ($order instanceof Order && $order->update($data)) {
+            $this->commitTransaction();
+            $this->withCode(200)->withMessage('编辑订单信息成功！');
+        } else {
+            $this->rollBackTransaction();
+            $this->withCode(500)->withError('没有对应的订单信息！');
+        }
     }
 }
