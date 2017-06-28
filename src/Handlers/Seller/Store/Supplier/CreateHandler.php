64617800cf42d3edc1780b40611f35798195ab10
twilroad <269044570@qq.com>
2017-06-28 14:33:13 +0800

    add logic of store supplier for seller

--- src/Handlers/Seller/Store/Supplier/CreateHandler.php
+++ src/Handlers/Seller/Store/Supplier/CreateHandler.php
@@ -9,6 +9,7 @@
 namespace Notadd\Mall\Handlers\Seller\Store\Supplier;
 
 use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Mall\Models\StoreSupplier;
 
 /**
  * Class CreateHandler.
@@ -22,6 +23,32 @@ class CreateHandler extends Handler
      */
     protected function execute()
     {
-        // TODO: Implement execute() method.
+        $this->validate($this->request, [
+            'store_id'  => 'required|numeric',
+            'name'      => 'required',
+            'contacts'  => 'required',
+            'telephone' => 'required',
+        ], [
+            'store_id.numeric'   => '店铺 ID 必须为数值',
+            'store_id.required'  => '店铺 ID 必须填写',
+            'name.required'      => '供货商名称必须填写',
+            'contacts.required'  => '联系人必须填写',
+            'telephone.required' => '联系电话必须填写',
+        ]);
+        $this->beginTransaction();
+        $data = $this->request->only([
+            'store_id',
+            'name',
+            'contacts',
+            'telephone',
+            'comments',
+        ]);
+        if (StoreSupplier::query()->create($data)) {
+            $this->commitTransaction();
+            $this->withCode(200)->withMessage('创建供应商成功！');
+        } else {
+            $this->rollBackTransaction();
+            $this->withCode(500)->withError('创建供应商失败！');
+        }
     }
 }
