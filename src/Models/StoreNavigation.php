810b50d278cd94b986e33ed6f17bed1396fc06d6
twilroad <269044570@qq.com>
2017-06-29 11:42:24 +0800

    add model for table mall_store_navigations

---
+++ src/Models/StoreNavigation.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime 2017-06-29 11:41
+ */
+namespace Notadd\Mall\Models;
+
+use Notadd\Foundation\Database\Model;
+
+/**
+ * Class StoreNavigation.
+ */
+class StoreNavigation extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = [];
+
+    /**
+     * @var string
+     */
+    protected $table = 'mall_store_navigations';
+}
