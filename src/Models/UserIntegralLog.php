34d6f2c95de8b4b219b3e543fd740d666526e27c
twilroad <269044570@qq.com>
2017-06-29 15:18:38 +0800

    add model for table mall_user_integral_logs

---
+++ src/Models/UserIntegralLog.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime 2017-06-29 15:17
+ */
+namespace Notadd\Mall\Models;
+
+use Notadd\Foundation\Database\Model;
+
+/**
+ * Class UserIntegralLog.
+ */
+class UserIntegralLog extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'comment',
+        'integral',
+        'user_id',
+    ];
+
+    /**
+     * @var string
+     */
+    protected $table = 'mall_user_integral_logs';
+}
