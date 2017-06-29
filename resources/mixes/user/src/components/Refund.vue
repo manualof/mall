dddbd74593f947de9f936d45f6433143ad8d8ca3
shx <857159145@qq.com>
2017-06-29 15:18:58 +0800

    data

--- resources/mixes/user/src/components/Refund.vue
+++ resources/mixes/user/src/components/Refund.vue
@@ -11,27 +11,27 @@
 
                         <div class="goods-img"></div>
                         <div class="goods-msg">
-                            <div >女装，夏季自然腰宽松九分裤休闲长裤麻混坊</div>
-                            <div class="goods-type">官方套餐</div>
+                            <div >{{ goods.msg }}</div>
+                            <div class="goods-type">{{ goods.taocan }}</div>
                         </div>
                     </div>
                 </div>
                 <div class="clearfix"></div>
                 <div class="refund-main">
-                    <p>运费：</p>
-                    <p>订单金额：</p>
-                    <p>订单编号：</p>
-                    <p>商家：</p>
+                    <p>运费： {{ goods.freight}}</p>
+                    <p>订单金额：<span class="price">￥{{goods.price }}</span></p>
+                    <p>订单编号：{{ goods.ordernum}}</p>
+                    <p>商家：{{ goods.seller}}</p>
                 </div>
             </div>
             <div class="shengqing-maijia">
                 <div class="title">买家退款申请</div>
                 <div class="maijia-box">
                     <div class="maijia-main">
-                        <p><span class="msg-title">退款原因</span><span class="msg-main">物品有瑕疵</span></p>
-                        <p><span class="msg-title">退款金额</span><span class="msg-main price">￥99</span></p>
-                        <p><span class="msg-title">退款编号</span><span class="msg-main">3265646123655</span></p>
-                        <p><span class="msg-title">退款说明</span><span class="msg-main">桌子的木质不太好，且桌面有2cm裂缝，桌面不平整，颜色发错。</span></p>
+                        <p><span class="msg-title">退款原因</span><span class="msg-main">{{ refund.reason }}</span></p>
+                        <p><span class="msg-title">退款金额</span><span class="msg-main price">￥{{ refund.price }}</span></p>
+                        <p><span class="msg-title">退款编号</span><span class="msg-main">{{ refund.number }}</span></p>
+                        <p><span class="msg-title">退款说明</span><span class="msg-main">{{ refund.illustrate }}</span></p>
                     </div>
                     <div class="shangjiachuli">
                         <p class="title-jinggao">
@@ -52,7 +52,20 @@
     export default{
         data() {
             return {
-                msg: '111',
+                goods: {
+                    freight: 10,
+                    price: 99,
+                    ordernum: '25648946541656',
+                    seller: 'xxx旗舰店',
+                    msg: '女装 夏季自然腰宽松九分裤休闲长裤麻混纺',
+                    taocan: '官方套餐一'
+                },
+                refund: {
+                    reason: '物品有瑕疵',
+                    price: 99,
+                    number: '3265646123655',
+                    illustrate: '桌子的木质不太好，且桌面有2cm裂缝，桌面不平整，颜色发错',
+                },
             };
         },
     };
