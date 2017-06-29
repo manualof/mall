1e766e0405d223c7cc397c3b8d7aade3a5c5b8e6
twilroad <269044570@qq.com>
2017-06-29 12:10:33 +0800

    add logic of store navigation list for seller

--- src/Handlers/Seller/Store/Navigation/ListHandler.php
+++ src/Handlers/Seller/Store/Navigation/ListHandler.php
@@ -9,6 +9,7 @@
 namespace Notadd\Mall\Handlers\Seller\Store\Navigation;
 
 use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Mall\Models\StoreNavigation;
 
 /**
  * Class ListHandler.
@@ -22,6 +23,26 @@ class ListHandler extends Handler
      */
     protected function execute()
     {
-        // TODO: Implement execute() method.
+        $this->validate($this->request, [
+            'page'     => 'numeric',
+            'paginate' => 'numeric',
+        ], [
+            'page.numeric'     => '当前页面必须为数值',
+            'paginate.numeric' => '分页数必须为数值',
+        ]);
+        $builder = StoreNavigation::query();
+        $builder = $builder->paginate($this->request->input('paginate', 20));
+        $this->withCode(200)->withData($builder->items())->withMessage('获取店铺导航列表成功！')->withExtra([
+            'pagination' => [
+                'total'         => $builder->total(),
+                'per_page'      => $builder->perPage(),
+                'current_page'  => $builder->currentPage(),
+                'last_page'     => $builder->lastPage(),
+                'next_page_url' => $builder->nextPageUrl(),
+                'prev_page_url' => $builder->previousPageUrl(),
+                'from'          => $builder->firstItem(),
+                'to'            => $builder->lastItem(),
+            ],
+        ]);
     }
 }
