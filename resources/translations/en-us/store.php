904324a4c48d960520560ac6097918085fc41210
twilroad <269044570@qq.com>
2017-06-30 17:17:23 +0800

    multi-language support

--- resources/translations/en-us/store.php
+++ resources/translations/en-us/store.php
@@ -4,7 +4,55 @@
  *
  * @author TwilRoad <heshudong@ibenchu.com>
  * @copyright (c) 2017, notadd.com
- * @datetime 2017-05-27 13:15
+ * @datetime 2017-05-27 13:16
  */
-
-return [];
+return [
+    'category'   => [
+        'category' => [
+            'fail'    => '',
+            'success' => '',
+        ],
+        'list'     => [
+            'fail'    => '',
+            'success' => '',
+        ],
+    ],
+    'navigation' => [
+        'list' => [
+            'fail'    => '',
+            'success' => '',
+        ],
+    ],
+    'product'    => [
+        'product' => [
+            'list'    => [
+                'fail'    => '',
+                'success' => '',
+            ],
+            'product' => [
+                'fail'    => '',
+                'success' => '',
+            ],
+        ],
+        'rate'    => [
+            'list' => [
+                'fail'    => '',
+                'success' => '',
+            ],
+            'rate' => [
+                'fail'    => '',
+                'success' => '',
+            ],
+        ],
+    ],
+    'store'      => [
+        'list'  => [
+            'fail'    => '',
+            'success' => '',
+        ],
+        'store' => [
+            'fail'    => '',
+            'success' => '',
+        ],
+    ],
+];
