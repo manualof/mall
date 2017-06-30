823787c393e9d1067872490414bba63a988e05d2
twilroad <269044570@qq.com>
2017-06-30 14:30:50 +0800

    add router, entry, and handler for api footprint for user

---
+++ src/Handlers/User/Footprint/FootprintHandler.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime 2017-06-30 14:24
+ */
+namespace Notadd\Mall\Handlers\User\Footprint;
+
+use Notadd\Foundation\Routing\Abstracts\Handler;
+
+/**
+ * Class FootprintHandler.
+ */
+class FootprintHandler extends Handler
+{
+    /**
+     * Execute Handler.
+     *
+     * @throws \Exception
+     */
+    protected function execute()
+    {
+        // TODO: Implement execute() method.
+    }
+}
