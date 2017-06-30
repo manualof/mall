0ed1d4c7866861166ad3cdf31cfdd6917cb5d960
twilroad <269044570@qq.com>
2017-06-30 15:24:19 +0800

    add logic of user footprint for user

--- src/Handlers/User/Footprint/FootprintHandler.php
+++ src/Handlers/User/Footprint/FootprintHandler.php
@@ -9,6 +9,7 @@
 namespace Notadd\Mall\Handlers\User\Footprint;
 
 use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Mall\Models\UserFootprint;
 
 /**
  * Class FootprintHandler.
@@ -22,6 +23,17 @@ class FootprintHandler extends Handler
      */
     protected function execute()
     {
-        // TODO: Implement execute() method.
+        $this->validate($this->request, [
+            'id' => 'required|numeric',
+        ], [
+            'id.numeric'  => '足迹 ID 必须为数值',
+            'id.required' => '足迹 ID 必须填写',
+        ]);
+        $footprint = UserFootprint::query()->find($this->request->input('id'));
+        if ($footprint instanceof UserFootprint) {
+            $this->withCode(200)->withData($footprint)->withMessage('获取足迹信息成功！');
+        } else {
+            $this->withCode(500)->withError('没有对应的足迹信息！');
+        }
     }
 }
