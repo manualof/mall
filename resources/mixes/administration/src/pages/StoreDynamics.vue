11363081d242b459e50964f3a9e9eb0fc2233963
TianJin <tfanxi@163.com>
2017-07-03 16:47:18 +0800

    add store dynamics module

--- resources/mixes/administration/src/pages/StoreDynamics.vue
+++ resources/mixes/administration/src/pages/StoreDynamics.vue
@@ -7,8 +7,163 @@
                 injection.sidebar.active('mall');
             });
         },
+        data() {
+            return {
+                brandColumns: [
+                    {
+                        align: 'center',
+                        type: 'selection',
+                        width: 60,
+                    },
+                    {
+                        align: 'center',
+                        key: 'title',
+                        title: '动态标题',
+                        width: 180,
+                    },
+                    {
+                        align: 'center',
+                        key: 'name',
+                        title: '店铺名称',
+                        width: 180,
+                    },
+                    {
+                        align: 'center',
+                        key: 'id',
+                        title: '店铺ID',
+                    },
+                    {
+                        align: 'center',
+                        key: 'isshow',
+                        render(row) {
+                            return `<span v-if="${row.status} === true" class="status-check">
+                                    <icon type="checkmark-circled"></icon>是</span>
+                                    <span v-if="${row.status} === false">
+                                    <icon type="close-circled"></icon>否</span>`;
+                        },
+                        title: '是否推荐品牌',
+                    },
+                    {
+                        align: 'center',
+                        key: 'time',
+                        title: '发表时间',
+                    },
+                    {
+                        align: 'center',
+                        key: 'num',
+                        title: '转播数量',
+                    },
+                    {
+                        align: 'center',
+                        key: 'account',
+                        title: '评论数量',
+                    },
+                    {
+                        align: 'center',
+                        key: 'action',
+                        render(row, column, index) {
+                            return `<i-button size="small" type="ghost" @click.native="remove(${index})">删除</i-button>
+                                    <i-button size="small" type="ghost" @click.native="edit">设置</i-button>`;
+                        },
+                        title: '操作',
+                        width: 180,
+                    },
+                ],
+                brandData: [
+                    {
+                        account: 324,
+                        id: 3,
+                        isshow: '是',
+                        name: 'karcher凯驰旗舰店',
+                        num: 42,
+                        status: true,
+                        time: '2016-12-12  16:11:27',
+                        title: '亲，我家又上新款了。',
+                    },
+                    {
+                        account: 324,
+                        id: 3,
+                        isshow: '是',
+                        name: 'karcher凯驰旗舰店',
+                        num: 42,
+                        status: true,
+                        time: '2016-12-12  16:11:27',
+                        title: '亲，我家又上新款了。',
+                    },
+                    {
+                        account: 324,
+                        id: 3,
+                        isshow: '是',
+                        name: 'karcher凯驰旗舰店',
+                        num: 42,
+                        status: true,
+                        time: '2016-12-12  16:11:27',
+                        title: '亲，我家又上新款了。',
+                    },
+                    {
+                        account: 324,
+                        id: 3,
+                        isshow: '是',
+                        name: 'karcher凯驰旗舰店',
+                        num: 42,
+                        status: true,
+                        time: '2016-12-12  16:11:27',
+                        title: '亲，我家又上新款了。',
+                    },
+                ],
+                self: this,
+            };
+        },
+        methods: {
+            edit() {
+                const self = this;
+                self.$router.push({
+                    path: 'brand/edit',
+                });
+            },
+            exportData() {
+                this.$refs.brand.exportCsv({
+                    filename: '品牌管理数据',
+                });
+            },
+            newAddData() {
+                const self = this;
+                self.$router.push({
+                    path: 'brand/add',
+                });
+            },
+            remove(index) {
+                this.brandData.splice(index, 1);
+            },
+        },
     };
 </script>
 <template>
-
+    <div class="mall-wrap">
+        <div class="goods-dynamics">
+            <tabs value="name1">
+                <tab-pane label="店铺动态" name="name1">
+                    <card :bordered="false">
+                        <div class="prompt-box">
+                            <p>提示</p>
+                            <p>当店主添加商品时可选择商品品牌，用户可根据品牌查询商品列表
+                                被推荐品牌，将在前台品牌推荐模块显示</p>
+                            <p>在品牌列表页面，品牌将按类别分组，即具有相同类别的品牌为一组，品牌类别与品牌分类无联系</p>
+                        </div>
+                        <div class="brand-management">
+                            <i-button class="add-data" type="ghost" @click.native="newAddData">+新增数据</i-button>
+                            <i-button class="export-btn" @click="exportData" type="ghost">导出数据</i-button>
+                            <i-button type="text" icon="android-sync" class="refresh">刷新</i-button>
+                        </div>
+                        <i-table :columns="brandColumns"
+                                 :context="self"
+                                 :data="brandData"
+                                 highlight-row
+                                 ref="brand">
+                        </i-table>
+                    </card>
+                </tab-pane>
+            </tabs>
+        </div>
+    </div>
 </template>
