d0f07710e80b2f7dce0b4c806191803c3a1aec70
twilroad <269044570@qq.com>
2017-07-04 16:12:56 +0800

    refactor logic of store edit for administration

--- src/Handlers/Administration/Store/EditHandler.php
+++ src/Handlers/Administration/Store/EditHandler.php
@@ -23,11 +23,40 @@ class EditHandler extends Handler
      */
     public function execute()
     {
-        $id = $this->request->input('id');
-        $shop = Store::query()->find($id);
-        if ($shop && $shop->update($this->request->all())) {
+        $this->validate($this->request, [
+            'category_id' => 'numeric',
+            'id'          => 'required|numeric',
+            'name'        => 'required',
+            'status'      => 'in:review,opening,closed,banned',
+            'user_id'     => 'numeric',
+        ], [
+            'category_id.numeric' => '所属分类 ID 必须为数值',
+            'id.numeric'          => '店铺 ID 必须填写',
+            'id.required'         => '店铺 ID 必须为数值',
+            'name.required'       => '店铺名称必须填写',
+            'user_id.numeric'     => '店铺所有者 ID 必须为数值',
+            'status.in'           => '店铺状态不在允许的范围内',
+        ]);
+        $this->beginTransaction();
+        $data = $this->request->only([
+            'address',
+            'category_id',
+            'company',
+            'end_at',
+            'flow_marketing',
+            'level',
+            'location',
+            'name',
+            'open_at',
+            'status',
+            'user_id',
+        ]);
+        $store = Store::query()->find($this->request->input('id'));
+        if ($store instanceof Store && $store->update($data)) {
+            $this->commitTransaction();
             $this->withCode(200)->withMessage('');
         } else {
+            $this->rollBackTransaction();
             $this->withCode(500)->withError('');
         }
     }
