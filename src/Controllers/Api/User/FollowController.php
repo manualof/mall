d13505248535c88c4de806854a1c61b203e8b93d
twilroad <269044570@qq.com>
2017-07-05 21:03:27 +0800

    add router, entry, and handler for api follow/list for user

---
+++ src/Controllers/Api/User/FollowController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime 2017-07-05 18:10
+ */
+namespace Notadd\Mall\Controllers\Api\User;
+
+use Notadd\Foundation\Routing\Abstracts\Controller;
+
+/**
+ * Class FollowController.
+ */
+class FollowController extends Controller
+{
+}
