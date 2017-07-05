35053e911e2e4967af3ec4a148fcae6dcc41a3c5
twilroad <269044570@qq.com>
2017-07-05 16:52:51 +0800

    add router, entry, and handler for api subscribe/create for user

--- src/Controllers/Api/User/SubscribeController.php
+++ src/Controllers/Api/User/SubscribeController.php
@@ -9,6 +9,7 @@
 namespace Notadd\Mall\Controllers\Api\User;
 
 use Notadd\Foundation\Routing\Abstracts\Controller;
+use Notadd\Mall\Handlers\User\Subscribe\CreateHandler;
 use Notadd\Mall\Handlers\User\Subscribe\ListHandler;
 
 /**
@@ -16,6 +17,17 @@ use Notadd\Mall\Handlers\User\Subscribe\ListHandler;
  */
 class SubscribeController extends Controller
 {
+    /**
+     * @param \Notadd\Mall\Handlers\User\Subscribe\CreateHandler $handler
+     *
+     * @return \Notadd\Foundation\Passport\Responses\ApiResponse|\Psr\Http\Message\ResponseInterface|\Zend\Diactoros\Response
+     * @throws \Exception
+     */
+    public function create(CreateHandler $handler)
+    {
+        return $handler->toResponse()->generateHttpResponse();
+    }
+
     /**
      * @param \Notadd\Mall\Handlers\User\Subscribe\ListHandler $handler
      *
