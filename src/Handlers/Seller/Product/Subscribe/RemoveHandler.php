cee6e4289a4d8532fd67bb82188db9eddc612b58
twilroad <269044570@qq.com>
2017-07-06 17:31:04 +0800

    extend validate rule usages for seller product

--- src/Handlers/Seller/Product/Subscribe/RemoveHandler.php
+++ src/Handlers/Seller/Product/Subscribe/RemoveHandler.php
@@ -31,7 +31,7 @@ class RemoveHandler extends Handler
                 'required',
             ],
         ], [
-            'id.exists'   => 'mall_product_subscribes',
+            'id.exists'   => '没有对应的商品订阅信息',
             'id.required' => '订阅 ID 必须填写',
             'id.numeric'  => '订阅 ID 必须为数值',
         ]);
@@ -39,10 +39,10 @@ class RemoveHandler extends Handler
         $subscribe = ProductSubscribe::query()->find($this->request->input('id'));
         if ($subscribe instanceof ProductSubscribe && $subscribe->delete()) {
             $this->commitTransaction();
-            $this->withCode(200)->withMessage('删除订阅信息成功！');
+            $this->withCode(200)->withMessage('删除商品订阅信息成功！');
         } else {
             $this->rollBackTransaction();
-            $this->withCode(500)->withError('没有对应的订阅信息！');
+            $this->withCode(500)->withError('没有对应的商品订阅信息！');
         }
     }
 }
