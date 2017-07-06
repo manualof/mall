d709fc19a11f6160e8bd013d16505372704d1514
twilroad <269044570@qq.com>
2017-07-06 11:27:27 +0800

    add logic of cart add for user

--- src/Handlers/User/Card/AddHandler.php
+++ src/Handlers/User/Card/AddHandler.php
@@ -9,6 +9,7 @@
 namespace Notadd\Mall\Handlers\User\Card;
 
 use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Mall\Models\UserCart;
 
 /**
  * Class AddHandler.
@@ -22,6 +23,31 @@ class AddHandler extends Handler
      */
     public function execute()
     {
-        // TODO: Implement execute() method.
+        $this->validate($this->request, [
+            'product_id' => 'required|numeric',
+            'store_id'   => 'required|numeric',
+            'user_id'    => 'required|numeric',
+        ], [
+            'product_id.numeric'  => '商品 ID 必须为数值',
+            'product_id.required' => '商品 ID 必须填写',
+            'store_id.numeric'    => '店铺 ID 必须为数值',
+            'store_id.required'   => '店铺 ID 必须填写',
+            'user_id.numeric'     => '用户 ID 必须为数值',
+            'user_id.required'    => '用户 ID 必须填写',
+        ]);
+        $this->beginTransaction();
+        $data = $this->request->only([
+            'price',
+            'product_id',
+            'store_id',
+            'user_id',
+        ]);
+        if (UserCart::query()->create($data)) {
+            $this->commitTransaction();
+            $this->withCode(200)->withMessage('添加商品到购物车成功！');
+        } else {
+            $this->rollBackTransaction();
+            $this->withCode(500)->withError('添加商品到购物车失败！');
+        }
     }
 }
