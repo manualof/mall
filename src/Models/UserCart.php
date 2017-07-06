a3c3ec7853a8754a68d491aba788eb7a306f3e84
twilroad <269044570@qq.com>
2017-07-06 11:11:06 +0800

    add model for table mall_user_carts

---
+++ src/Models/UserCart.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime 2017-07-06 11:10
+ */
+namespace Notadd\Mall\Models;
+
+use Notadd\Foundation\Database\Model;
+
+/**
+ * Class UserCart.
+ */
+class UserCart extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = [
+    ];
+
+    /**
+     * @var string
+     */
+    protected $table = 'mall_user_carts';
+}
