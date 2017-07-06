49e5a83ad3c9941d051d2f1b743c7598a9942bc1
twilroad <269044570@qq.com>
2017-07-06 11:56:43 +0800

    add logic of cart empty for user

--- src/Handlers/User/Card/EmptyHandler.php
+++ src/Handlers/User/Card/EmptyHandler.php
@@ -8,7 +8,9 @@
  */
 namespace Notadd\Mall\Handlers\User\Card;
 
+use Illuminate\Database\Eloquent\Collection;
 use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Mall\Models\UserCart;
 
 /**
  * Class EmptyHandler.
@@ -22,6 +24,23 @@ class EmptyHandler extends Handler
      */
     public function execute()
     {
-        // TODO: Implement execute() method.
+        $this->validate($this->request, [
+            'user_id' => 'required|numeric',
+        ], [
+            'user_id.numeric'  => '用户 ID 必须为数值',
+            'user_id.required' => '用户 ID 必须填写',
+        ]);
+        $this->beginTransaction();
+        $cart = UserCart::query()->where('user_id', $this->request->input('user_id'))->get();
+        if ($cart instanceof Collection && $cart->every(function (UserCart $record) {
+                return $record->delete();
+            })
+        ) {
+            $this->commitTransaction();
+            $this->withCode(200)->withMessage('清空购物车成功！');
+        } else {
+            $this->rollBackTransaction();
+            $this->withCode(500)->withError('清空购物车失败！');
+        }
     }
 }
