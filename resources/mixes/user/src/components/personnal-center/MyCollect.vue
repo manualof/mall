736557a690ebfd6b5823e88dddcfd18f03bfae11
zhanghuan123 <zhhu_123@163.com>
2017-07-06 17:53:27 +0800

    Add MyCollect

---
+++ resources/mixes/user/src/components/personnal-center/MyCollect.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="my-collect">
+        <div class="collect-title">
+            <span>我的收藏</span>
+        </div>
+        <div class="clearfix"></div>
+        <div class="row">
+            <div  v-for="(item, index) in goods" class="col-sm-3">
+                <div>
+                    <router-link to="/search/product-details">
+                        <img :src="item.imgsrc" alt="">
+                    </router-link>
+                    <div class="goods-msg">
+                        <div class="price">
+                            <i>￥</i><span>{{ item.pricle }}</span>
+                            <span class="old-price">￥{{ item.oldprice }}</span>
+                        </div>
+                        <router-link to="/search/product-details" class="goods-name">
+                            {{ item.name }}
+                        </router-link>
+                        <div class="sales">
+                            <div class="month-sales">本月销量 <b>{{ item.num }} </b>件</div>
+                            <div @click.self="cancel(index)" class="cancel">取消收藏</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import image1 from '../../assets/images/img_07.png';
+
+    export default {
+        data() {
+            return {
+                goods: [
+                    {
+                        name: 'Apple iPhone 6 32G 金色 移动联通电信4G手机',
+                        imgsrc: image1,
+                        pricle: 3299,
+                        oldprice: 46.88,
+                        num: 3324,
+                    },
+                    {
+                        name: 'Apple iPhone 6 32G 金色 移动联通电信4G手机',
+                        imgsrc: image1,
+                        pricle: 3299,
+                        oldprice: 46.88,
+                        num: 3324,
+                    },
+                    {
+                        name: 'Apple iPhone 6 32G 金色 移动联通电信4G手机',
+                        imgsrc: image1,
+                        pricle: 3299,
+                        oldprice: 46.88,
+                        num: 3324,
+                    },
+                    {
+                        name: 'Apple iPhone 6 32G 金色 移动联通电信4G手机',
+                        imgsrc: image1,
+                        pricle: 3299,
+                        oldprice: 46.88,
+                        num: 3324,
+                    },
+                    {
+                        name: 'Apple iPhone 6 32G 金色 移动联通电信4G手机',
+                        imgsrc: image1,
+                        pricle: 3299,
+                        oldprice: 46.88,
+                        num: 3324,
+                    },
+                    {
+                        name: 'Apple iPhone 6 32G 金色 移动联通电信4G手机',
+                        imgsrc: image1,
+                        pricle: 3299,
+                        oldprice: 46.88,
+                        num: 3324,
+                    },
+                    {
+                        name: 'Apple iPhone 6 32G 金色 移动联通电信4G手机',
+                        imgsrc: image1,
+                        pricle: 3299,
+                        oldprice: 46.88,
+                        num: 3324,
+                    },
+                ],
+            };
+        },
+        methods: {
+            cancel(num) {
+                this.goods.splice(num, 1);
+            },
+        },
+    };
+</script>
