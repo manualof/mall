e1ac27a64ebfd0e4c3e34731e5119269f3feb1a6
zhanghuan123 <zhhu_123@163.com>
2017-07-07 12:28:51 +0800

    Add Notice

---
+++ resources/mixes/user/src/components/personnal-center/Notice.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="notice">
+        <h4>预约到货通知</h4>
+        <ul class="list-head clearfix">
+            <li>商品</li>
+            <li>操作</li>
+            <li>添加时间</li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+            };
+        },
+    };
+</script>
