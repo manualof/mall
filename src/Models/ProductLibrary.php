8341792602f8b10f99fb9c51b387517af51cc247
twilroad <269044570@qq.com>
2017-07-12 11:15:22 +0800

    add model for table mall_product_libraries

---
+++ src/Models/ProductLibrary.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime 2017-07-12 11:13
+ */
+namespace Notadd\Mall\Models;
+
+use Notadd\Foundation\Database\Model;
+
+/**
+ * Class ProductLibrary.
+ */
+class ProductLibrary extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = [];
+
+    /**
+     * @var string
+     */
+    protected $table = 'mall_product_libraries';
+}
