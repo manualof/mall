a8da3882989b7f49f5d95dee673e60428010a10f
twilroad <269044570@qq.com>
2017-07-12 12:35:17 +0800

    add router, entry, handler for product library create

---
+++ src/Controllers/Api/Administration/ProductLibraryController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime 2017-07-12 11:44
+ */
+namespace Notadd\Mall\Controllers\Api\Administration;
+
+use Notadd\Foundation\Routing\Abstracts\Controller;
+use Notadd\Mall\Handlers\Administration\Product\Library\CreateHandler;
+
+/**
+ * Class ProductLibraryController.
+ */
+class ProductLibraryController extends Controller
+{
+    /**
+     * @param \Notadd\Mall\Handlers\Administration\Product\Library\CreateHandler $handler
+     *
+     * @return \Notadd\Foundation\Passport\Responses\ApiResponse|\Psr\Http\Message\ResponseInterface|\Zend\Diactoros\Response
+     * @throws \Exception
+     */
+    public function create(CreateHandler $handler)
+    {
+        return $handler->toResponse()->generateHttpResponse();
+    }
+}
