6d9ccee9d437ef61c2a3764bf8c82c8cfaa9ee1e
TianJin <tfanxi@163.com>
2017-07-14 17:20:18 +0800

    replace date-picker methods for statistics module

--- resources/mixes/seller/src/pages/StatisticsFlow.vue
+++ resources/mixes/seller/src/pages/StatisticsFlow.vue
@@ -61,6 +61,37 @@
                         type: 'value',
                     },
                 },
+                orderMoneyOptions: {
+                    shortcuts: [
+                        {
+                            text: '最近一周',
+                            value() {
+                                const end = new Date();
+                                const start = new Date();
+                                start.setTime(start.getTime() - (3600 * 1000 * 24 * 7));
+                                return [start, end];
+                            },
+                        },
+                        {
+                            text: '最近一个月',
+                            value() {
+                                const end = new Date();
+                                const start = new Date();
+                                start.setTime(start.getTime() - (3600 * 1000 * 24 * 30));
+                                return [start, end];
+                            },
+                        },
+                        {
+                            text: '最近三个月',
+                            value() {
+                                const end = new Date();
+                                const start = new Date();
+                                start.setTime(start.getTime() - (3600 * 1000 * 24 * 90));
+                                return [start, end];
+                            },
+                        },
+                    ],
+                },
                 orderNumber: {
                     series: [
                         {
@@ -84,20 +115,6 @@
                 },
                 self: this,
                 style: 'height: 400px;',
-                timeList: [
-                    {
-                        label: '按照月统计',
-                        value: '1',
-                    },
-                    {
-                        label: '按照周统计',
-                        value: '2',
-                    },
-                    {
-                        label: '按照天统计',
-                        value: '3',
-                    },
-                ],
             };
         },
     };
@@ -118,13 +135,11 @@
                                     <ul>
                                         <li>
                                             时间周期
-                                            <i-select v-model="model2" style="width:124px">
-                                                <i-option v-for="item in timeList" :value="item.value"
-                                                          :key="item">{{ item.label }}</i-option>
-                                            </i-select>
-                                        </li>
-                                        <li>
-                                            <date-picker type="date" placeholder="选择日期"></date-picker>
+                                            <date-picker :options="orderMoneyOptions"
+                                                         placement="bottom-end"
+                                                         placeholder="选择日期"
+                                                         style="width: 200px"
+                                                         type="daterange"></date-picker>
                                         </li>
                                     </ul>
                                 </div>
@@ -150,13 +165,11 @@
                                     <ul>
                                         <li>
                                             时间周期
-                                            <i-select v-model="model2" style="width:124px">
-                                                <i-option v-for="item in timeList" :value="item.value"
-                                                          :key="item">{{ item.label }}</i-option>
-                                            </i-select>
-                                        </li>
-                                        <li>
-                                            <date-picker type="date" placeholder="选择日期"></date-picker>
+                                            <date-picker :options="orderMoneyOptions"
+                                                         placement="bottom-end"
+                                                         placeholder="选择日期"
+                                                         style="width: 200px"
+                                                         type="daterange"></date-picker>
                                         </li>
                                     </ul>
                                 </div>
