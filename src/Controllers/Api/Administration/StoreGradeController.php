2a252e6196e51b2563110107d5c5b2d22b97b9d3
twilroad <269044570@qq.com>
2017-07-15 19:45:25 +0800

    add router, entry, and handler for api store/grade/list for administration

---
+++ src/Controllers/Api/Administration/StoreGradeController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of Notadd.
+ *
+ * @author TwilRoad <269044570@qq.com>
+ * @copyright (c) 2017, notadd.com
+ * @datetime 2017-07-15 19:38
+ */
+namespace Notadd\Mall\Controllers\Api\Administration;
+
+use Notadd\Foundation\Routing\Abstracts\Controller;
+use Notadd\Mall\Handlers\Administration\Store\Grade\ListHandler;
+
+/**
+ * Class StoreGradeController.
+ */
+class StoreGradeController extends Controller
+{
+    /**
+     * @param \Notadd\Mall\Handlers\Administration\Store\Grade\ListHandler $handler
+     *
+     * @return \Notadd\Foundation\Passport\Responses\ApiResponse|\Psr\Http\Message\ResponseInterface|\Zend\Diactoros\Response
+     * @throws \Exception
+     */
+    public function list(ListHandler $handler)
+    {
+        return $handler->toResponse()->generateHttpResponse();
+    }
+}
