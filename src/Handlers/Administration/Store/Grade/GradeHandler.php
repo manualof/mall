ba776f16477148e83867c0b578ed75e735a45f80
twilroad <269044570@qq.com>
2017-07-15 19:54:58 +0800

    add logic of store grade for administration

--- src/Handlers/Administration/Store/Grade/GradeHandler.php
+++ src/Handlers/Administration/Store/Grade/GradeHandler.php
@@ -9,6 +9,8 @@
 namespace Notadd\Mall\Handlers\Administration\Store\Grade;
 
 use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Foundation\Validation\Rule;
+use Notadd\Mall\Models\StoreGrade;
 
 /**
  * Class GradeHandler.
@@ -22,6 +24,22 @@ class GradeHandler extends Handler
      */
     protected function execute()
     {
-        // TODO: Implement execute() method.
+        $this->validate($this->request, [
+            'id' => [
+                Rule::exists(''),
+                Rule::numeric(),
+                Rule::required(),
+            ],
+        ], [
+            'id.exists'   => '没有对应的店铺等级信息',
+            'id.numeric'  => '店铺等级 ID 必须为数值',
+            'id.required' => '店铺等级 ID 必须填写',
+        ]);
+        $grade = StoreGrade::query()->find($this->request->input('id'));
+        if ($grade instanceof StoreGrade) {
+            $this->withCode(200)->withData($grade)->withMessage('获取店铺等级信息成功！');
+        } else {
+            $this->withCode(500)->withError('没有对应的店铺等级信息！');
+        }
     }
 }
