8abca4206482507136cd11fe35950daf2f253379
zhanghuan123 <zhhu_123@163.com>
2017-07-19 23:59:16 +0800

    Add spike

--- resources/mixes/foreground/src/components/Spike.vue
+++ resources/mixes/foreground/src/components/Spike.vue
@@ -1,3 +1,39 @@
+<script>
+    export default {
+        data() {
+            return {
+                now: true,
+                times: [
+                    {
+                        time: '08:00',
+                    },
+                    {
+                        time: '12:00',
+                    },
+                    {
+                        time: '14:00',
+                    },
+                    {
+                        time: '16:00',
+                    },
+                    {
+                        time: '22:00',
+                    },
+                ],
+            };
+        },
+    };
+</script>
 <template>
-    <div>111</div>
+    <div class="spike">
+        <div class="container">
+            <ul class="time-list">
+                <li class="spike-time pull-left" v-for="item in times" :active="now">
+                    {{ item.time }}
+                    <i class="end" v-if="!now">已经结束</i>
+                    <i class="now" v-if="now">正在进行</i>
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
