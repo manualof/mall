e7b1adfd2647ceee40aeeb2f12944801e3584126
twilroad <269044570@qq.com>
2017-07-20 16:30:32 +0800

    add template codes and publish assets for foreground store

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -1,8 +1,19 @@
-<?php
-/**
- * This file is part of Notadd.
- *
- * @author TwilRoad <269044570@qq.com>
- * @copyright (c) 2017, notadd.com
- * @datetime 2017-06-19 21:20
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=utf-8>
+    <title>notadd-mall-store</title>
+    <link href="{{ asset('assets/mall/store/css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div id="app"></div>
+<script>
+    window.userLink = 'http://localhost:8080/';
+    window.StoreLink = 'http://localhost:8081/';
+    window.foregroundLink = 'http://localhost:8082/';
+</script>
+<script type="text/javascript" src="{{ asset('assets/mall/store/js/manifest.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/mall/store/js/vendor.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/mall/store/js/app.js') }}"></script>
+</body>
+</html>
--- src/ModuleServiceProvider.php
+++ src/ModuleServiceProvider.php
@@ -44,6 +44,7 @@ class ModuleServiceProvider extends Module
             realpath(__DIR__ . '/../resources/mixes/administration/dist/assets/mall/administration') => public_path('assets/mall/administration'),
             realpath(__DIR__ . '/../resources/mixes/foreground/dist/assets/mall/mall') => public_path('assets/mall/mall'),
             realpath(__DIR__ . '/../resources/mixes/seller/dist/assets/mall/seller') => public_path('assets/mall/seller'),
+            realpath(__DIR__ . '/../resources/mixes/store/dist/assets/mall/store') => public_path('assets/mall/store'),
         ], 'public');
     }
 
