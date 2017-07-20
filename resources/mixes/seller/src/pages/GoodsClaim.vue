12e8c0aef8b560698d41ee9658ee71a3e0956bb6
TianJin <tfanxi@163.com>
2017-07-20 10:59:24 +0800

    add goods claim modal module

--- resources/mixes/seller/src/pages/GoodsClaim.vue
+++ resources/mixes/seller/src/pages/GoodsClaim.vue
@@ -50,12 +50,12 @@
                     {
                         align: 'center',
                         key: 'action',
-                        render(h, data) {
+                        render(h) {
                             return h('div', [
                                 h('i-button', {
                                     on: {
                                         click() {
-                                            self.remove(data.index);
+                                            self.modal = true;
                                         },
                                     },
                                     props: {
@@ -69,6 +69,12 @@
                         width: 180,
                     },
                 ],
+                form: {
+                    freight: '',
+                    market_price: '',
+                    price: '',
+                    stock: '',
+                },
                 list: [
                     {
                         category: '电子产品',
@@ -86,6 +92,8 @@
                         name: '可用十年 创意椭圆不锈钢肥皂(带底托精装) 神奇除味去',
                     },
                 ],
+                loading: false,
+                modal: false,
                 searchList: [
                     {
                         label: '商品名称',
@@ -98,7 +106,26 @@
                 ],
             };
         },
-        methods: {},
+        methods: {
+            goBack() {
+                const self = this;
+                self.$router.go(-1);
+            },
+            submit() {
+                const self = this;
+                self.loading = true;
+                self.$refs.form.validate(valid => {
+                    if (valid) {
+                        window.console.log(valid);
+                    } else {
+                        self.loading = false;
+                        self.$notice.error({
+                            title: '请正确填写设置信息！',
+                        });
+                    }
+                });
+            },
+        },
     };
 </script>
 <template>
@@ -131,6 +158,58 @@
                     <page :total="100" show-elevator></page>
                 </div>
             </card>
+            <modal
+                    v-model="modal"
+                    title="认领商品" class="goods-claim-modal upload-picture-modal">
+                <div>
+                    <i-form :label-width="100" :model="form" ref="form" :rules="rules">
+                        <form-item label="商品价格">
+                            <row>
+                                <i-col span="4">
+                                    <i-input v-model="form.price"></i-input>
+                                </i-col>
+                                <i-col span="2">元</i-col>
+                            </row>
+                            <p class="tip">价格必须是0.01~9999999之间的数字，且不能高于市场价</p>
+                        </form-item>
+                        <form-item label="市场价格">
+                            <row>
+                                <i-col span="4">
+                                    <i-input v-model="form.market_price"></i-input>
+                                </i-col>
+                                <i-col span="2">元</i-col>
+                            </row>
+                            <p class="tip">价格必须是0.01~9999999之间的数字，此价格仅为市场参考售价，请根据该实际情况认真填写</p>
+                        </form-item>
+                        <form-item label="商品库存">
+                            <row>
+                                <i-col span="4">
+                                    <i-input v-model="form.stock"></i-input>
+                                </i-col>
+                            </row>
+                            <p class="tip">商铺库存数量必须为0~9999999999之间的整数</p>
+                        </form-item>
+                        <form-item label="固定运费">
+                            <row>
+                                <i-col span="4">
+                                    <i-input v-model="form.freight"></i-input>
+                                </i-col>
+                                <i-col span="2">元</i-col>
+                            </row>
+                        </form-item>
+                        <row>
+                            <i-col span="20">
+                                <form-item>
+                                    <i-button :loading="loading" type="primary" @click.native="submit">
+                                        <span v-if="!loading">确认提交</span>
+                                        <span v-else>正在提交…</span>
+                                    </i-button>
+                                </form-item>
+                            </i-col>
+                        </row>
+                    </i-form>
+                </div>
+            </modal>
         </div>
     </div>
 </template>
