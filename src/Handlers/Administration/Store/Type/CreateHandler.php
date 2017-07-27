5d26e6e59c4045fe9b5135a192d769921cbb407a
twilroad <269044570@qq.com>
2017-07-27 20:35:17 +0800

    add logic for store type create api for administration

--- src/Handlers/Administration/Store/Type/CreateHandler.php
+++ src/Handlers/Administration/Store/Type/CreateHandler.php
@@ -9,6 +9,8 @@
 namespace Notadd\Mall\Handlers\Administration\Store\Type;
 
 use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Foundation\Validation\Rule;
+use Notadd\Mall\Models\StoreType;
 
 /**
  * Class CreateHandler.
@@ -22,6 +24,27 @@ class CreateHandler extends Handler
      */
     protected function execute()
     {
-        // TODO: Implement execute() method.
+        $this->validate($this->request, [
+            'amount_of_deposit' => Rule::numeric(),
+            'name'              => Rule::required(),
+            'order'             => Rule::numeric(),
+        ], [
+            'amount_of_deposit.numeric' => '保证金数额必须为数值',
+            'name.required'             => '类型名称必须填写',
+            'order.numeric'             => '排序必须为数值',
+        ]);
+        $this->beginTransaction();
+        $data = $this->request->only([
+            'amount_of_deposit',
+            'name',
+            'order',
+        ]);
+        if (StoreType::query()->create($data)) {
+            $this->commitTransaction();
+            $this->withCode(200)->withMessage('添加店铺类型信息成功！');
+        } else {
+            $this->rollBackTransaction();
+            $this->withCode(500)->withError('添加店铺类型信息失败！');
+        }
     }
 }
