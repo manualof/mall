3042dfd72d19c64ac128dfbd6c0368f75b305164
twilroad <269044570@qq.com>
2017-07-27 11:45:49 +0800

    add logic of store level create for administration

--- resources/mixes/administration/src/pages/StoreGradeCreate.vue
+++ resources/mixes/administration/src/pages/StoreGradeCreate.vue
@@ -12,17 +12,9 @@
                 action: `${window.api}/mall/admin/upload`,
                 loading: false,
                 rules: {
-                    apply: [
+                    application_instruction: [
                         {
                             message: '申请说明不能为空',
-                            required: true,
-                            trigger: 'blur',
-                            type: 'string',
-                        },
-                    ],
-                    charges: [
-                        {
-                            message: '收费标准不能为空',
                             required: true,
                             trigger: 'blur',
                             type: 'string',
@@ -33,10 +25,10 @@
                             message: '级别不能为空',
                             required: true,
                             trigger: 'blur',
-                            type: 'string',
+                            type: 'number',
                         },
                     ],
-                    levelName: [
+                    name: [
                         {
                             message: '等级名称不能为空',
                             required: true,
@@ -44,17 +36,25 @@
                             type: 'string',
                         },
                     ],
+                    price: [
+                        {
+                            message: '收费标准不能为空',
+                            required: true,
+                            trigger: 'blur',
+                            type: 'number',
+                        },
+                    ],
                 },
                 form: {
-                    apply: '',
-                    charges: '',
-                    goods: true,
-                    level: '',
-                    levelName: '',
-                    release: true,
-                    releaseNum: '',
-                    uploadNum: '',
-                    useType: [],
+                    application_instruction: '',
+                    can_claim: true,
+                    can_upload: true,
+                    extensions: [],
+                    level: 0,
+                    name: '',
+                    price: 0,
+                    publish_limit: 0,
+                    upload_limit: 0,
                 },
             };
         },
@@ -64,7 +64,18 @@
                 self.loading = true;
                 self.$refs.form.validate(valid => {
                     if (valid) {
-                        window.console.log(valid);
+                        self.$http.post(`${window.api}/mall/admin/store/grade/create`, self.form).then(() => {
+                            self.$notice.open({
+                                title: '添加店铺等级信息成功！',
+                            });
+                            self.$router.push('/mall/store/grade');
+                        }).catch(() => {
+                            self.$notice.error({
+                                title: '添加店铺等级信息失败！',
+                            });
+                        }).finally(() => {
+                            self.loading = false;
+                        });
                     } else {
                         self.loading = false;
                         self.$notice.error({
@@ -91,31 +102,31 @@
                 <i-form ref="form" :model="form" :rules="rules" :label-width="200">
                     <row>
                         <i-col span="12">
-                            <form-item label="等级名称" prop="levelName">
-                                <i-input v-model="form.levelName"></i-input>
+                            <form-item label="等级名称" prop="name">
+                                <i-input v-model="form.name"></i-input>
                             </form-item>
                         </i-col>
                     </row>
                     <row>
                         <i-col span="12">
                             <form-item label="可发布商品数">
-                                <i-input v-model="form.releaseNum"></i-input>
+                                <i-input number v-model="form.publish_limit"></i-input>
                                 <p class="tip">0表示没有限制</p>
                             </form-item>
                         </i-col>
                     </row>
                     <row>
                         <i-col span="12">
-                            <form-item label="可上传商品数" prop="uploadNum">
-                                <i-input v-model="form.uploadNum"></i-input>
+                            <form-item label="可上传商品数" prop="upload_limit">
+                                <i-input number v-model="form.upload_limit"></i-input>
                                 <p class="tip">0表示没有限制</p>
                             </form-item>
                         </i-col>
                     </row>
                     <row>
                         <i-col span="12">
-                            <form-item label="可认领商品" prop="goods">
-                                <i-switch size="large" v-model="form.goods">
+                            <form-item label="可认领商品" prop="can_claim">
+                                <i-switch size="large" v-model="form.can_claim">
                                     <span slot="open">开启</span>
                                     <span slot="close">关闭</span>
                                 </i-switch>
@@ -124,8 +135,8 @@
                     </row>
                     <row>
                         <i-col span="12">
-                            <form-item label="可自主发布商品" prop="release">
-                                <i-switch size="large" v-model="form.release">
+                            <form-item label="可自主发布商品" prop="can_upload">
+                                <i-switch size="large" v-model="form.can_upload">
                                     <span slot="open">开启</span>
                                     <span slot="close">关闭</span>
                                 </i-switch>
@@ -134,16 +145,16 @@
                     </row>
                     <row>
                         <i-col span="12">
-                            <form-item label="收费标准" prop="charges">
-                                <i-input v-model="form.charges"></i-input>
+                            <form-item label="收费标准" prop="price">
+                                <i-input number v-model="form.price"></i-input>
                                 <p class="tip">收费标准，单位：元/年，必须为数字，在会员开通或升级店铺时将显示在前台</p>
                             </form-item>
                         </i-col>
                     </row>
                     <row>
                         <i-col span="12">
-                            <form-item label="可使用插件" prop="useType">
-                                <checkbox-group v-model="form.useType">
+                            <form-item label="可使用插件" prop="extensions">
+                                <checkbox-group v-model="form.extensions">
                                     <checkbox label="秒杀活动"></checkbox>
                                     <checkbox label="预售活动"></checkbox>
                                     <checkbox label="满减活动"></checkbox>
@@ -154,8 +165,8 @@
                     </row>
                     <row>
                         <i-col span="12">
-                            <form-item label="申请说明" prop="apply">
-                                <i-input v-model="form.apply" type="textarea"></i-input>
+                            <form-item label="申请说明" prop="application_instruction">
+                                <i-input v-model="form.application_instruction" type="textarea"></i-input>
                                 <p class="tip">在会员开通或升级店铺时将显示在前台</p>
                             </form-item>
                         </i-col>
@@ -163,7 +174,7 @@
                     <row>
                         <i-col span="12">
                             <form-item label="级别" prop="level">
-                                <i-input v-model="form.level"></i-input>
+                                <i-input number v-model="form.level"></i-input>
                                 <p class="tip">数值越大表明级别越高</p>
                             </form-item>
                         </i-col>
