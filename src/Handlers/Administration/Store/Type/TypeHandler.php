34857ab9b420251a8e5afb840b6f6570d471b2e2
twilroad <269044570@qq.com>
2017-07-28 12:29:47 +0800

    add logic for store type api for administration

--- src/Handlers/Administration/Store/Type/TypeHandler.php
+++ src/Handlers/Administration/Store/Type/TypeHandler.php
@@ -9,6 +9,8 @@
 namespace Notadd\Mall\Handlers\Administration\Store\Type;
 
 use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Foundation\Validation\Rule;
+use Notadd\Mall\Models\StoreType;
 
 /**
  * Class TypeHandler.
@@ -22,6 +24,22 @@ class TypeHandler extends Handler
      */
     protected function execute()
     {
-        // TODO: Implement execute() method.
+        $this->validate($this->request, [
+            'id' => [
+                Rule::exists('mall_store_types'),
+                Rule::numeric(),
+                Rule::required(),
+            ],
+        ], [
+            'id.exists'   => '没有对应的店铺类型信息',
+            'id.numeric'  => '店铺类型 ID 必须为数值',
+            'id.required' => '店铺类型 ID 必须填写',
+        ]);
+        $grade = StoreType::query()->find($this->request->input('id'));
+        if ($grade instanceof StoreType) {
+            $this->withCode(200)->withData($grade)->withMessage('获取店铺类型信息成功！');
+        } else {
+            $this->withCode(500)->withError('没有对应的店铺类型信息！');
+        }
     }
 }
