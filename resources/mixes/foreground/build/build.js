70e3e5b547da8cf03170142825a65173f8bf644c
twilroad <269044570@qq.com>
2017-07-28 17:08:45 +0800

    rebuild assets files

--- resources/mixes/foreground/build/build.js
+++ resources/mixes/foreground/build/build.js
@@ -2,21 +2,21 @@ require('./check-versions')();
 
 process.env.NODE_ENV = 'production';
 
-var ora = require('ora');
 var rm = require('rimraf');
 var path = require('path');
 var chalk = require('chalk');
 var webpack = require('webpack');
 var config = require('../config');
-var webpackConfig = require('./webpack.prod.conf');
-
-var spinner = ora('building for production...');
-spinner.start();
+var buildWebpackConfig = require('./webpack.prod.conf');
+var webpackConfig = merge(buildWebpackConfig, {
+    plugins: [
+        new webpack.ProgressPlugin(),
+    ],
+});
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     if (err) throw err;
     webpack(webpackConfig, function (err, stats) {
-        spinner.stop();
         if (err) throw err;
         process.stdout.write(stats.toString({
             colors: true,
