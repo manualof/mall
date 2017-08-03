2c6ce5c69e809abe8fbbdb27a120ca926640a1ed
twilroad <269044570@qq.com>
2017-08-03 13:56:28 +0800

    add validate rules for pay configuration logic for administration

--- src/Handlers/Administration/Configuration/Pay/SetHandler.php
+++ src/Handlers/Administration/Configuration/Pay/SetHandler.php
@@ -8,13 +8,32 @@
  */
 namespace Notadd\Mall\Handlers\Administration\Configuration\Pay;
 
+use Illuminate\Container\Container;
 use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Foundation\Setting\Contracts\SettingsRepository;
 
 /**
  * Class SetHandler.
  */
 class SetHandler extends Handler
 {
+    /**
+     * @var \Notadd\Foundation\Setting\Contracts\SettingsRepository
+     */
+    protected $settings;
+
+    /**
+     * SetHandler constructor.
+     *
+     * @param \Illuminate\Container\Container                         $container
+     * @param \Notadd\Foundation\Setting\Contracts\SettingsRepository $settings
+     */
+    public function __construct(Container $container, SettingsRepository $settings)
+    {
+        parent::__construct($container);
+        $this->settings = $settings;
+    }
+
     /**
      * Execute Handler.
      *
