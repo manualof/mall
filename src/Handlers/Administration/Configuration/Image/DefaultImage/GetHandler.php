f6178dbcff383bd2bd5686d6fb502464a34940b9
twilroad <269044570@qq.com>
2017-08-03 13:55:40 +0800

    add validate rules for image configuration logic for administration

--- src/Handlers/Administration/Configuration/Image/DefaultImage/GetHandler.php
+++ src/Handlers/Administration/Configuration/Image/DefaultImage/GetHandler.php
@@ -8,13 +8,32 @@
  */
 namespace Notadd\Mall\Handlers\Administration\Configuration\Image\DefaultImage;
 
+use Illuminate\Container\Container;
 use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Foundation\Setting\Contracts\SettingsRepository;
 
 /**
  * Class WatchHandler.
  */
 class GetHandler extends Handler
 {
+    /**
+     * @var \Notadd\Foundation\Setting\Contracts\SettingsRepository
+     */
+    protected $settings;
+
+    /**
+     * SetHandler constructor.
+     *
+     * @param \Illuminate\Container\Container                         $container
+     * @param \Notadd\Foundation\Setting\Contracts\SettingsRepository $settings
+     */
+    public function __construct(Container $container, SettingsRepository $settings)
+    {
+        parent::__construct($container);
+        $this->settings = $settings;
+    }
+
     /**
      * Execute Handler.
      *
