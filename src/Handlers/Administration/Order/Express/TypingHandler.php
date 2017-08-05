208d969c4360c17b2b6ba0cf3aabc9a7fea19b27
twilroad <269044570@qq.com>
2017-08-05 18:04:41 +0800

    add data validate for order express for administration

--- src/Handlers/Administration/Order/Express/TypingHandler.php
+++ src/Handlers/Administration/Order/Express/TypingHandler.php
@@ -9,6 +9,7 @@
 namespace Notadd\Mall\Handlers\Administration\Order\Express;
 
 use Notadd\Foundation\Routing\Abstracts\Handler;
+use Notadd\Foundation\Validation\Rule;
 use Notadd\Mall\Models\OrderExpress;
 
 /**
@@ -23,12 +24,22 @@ class TypingHandler extends Handler
      */
     public function execute()
     {
-        $id = $this->request->input('id');
-        $exchange = OrderExpress::query()->find($id);
-        if ($exchange) {
-            $this->withCode(200)->withMessage('');
+        $this->validate($this->request, [
+            'id' => [
+                Rule::exists('mall_order_expresses'),
+                Rule::numeric(),
+                Rule::required(),
+            ],
+        ], [
+            'id.exists'   => '没有对应的订单物流信息',
+            'id.numeric'  => '订单物流 ID 必须为数值',
+            'id.required' => '订单物流 ID 必须填写',
+        ]);
+        $exchange = OrderExpress::query()->find($this->request->input('id'));
+        if ($exchange instanceof OrderExpress) {
+            $this->withCode(200)->withMessage('更新订单物流信息成功！');
         } else {
-            $this->withCode(500)->withError('');
+            $this->withCode(500)->withError('更新订单物流信息失败！');
         }
     }
 }
