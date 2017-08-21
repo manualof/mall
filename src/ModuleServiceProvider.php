f1749d28e0c5b797595f2b67d9ff0771e496fda3
twilroad <269044570@qq.com>
2017-08-21 22:39:29 +0800

    remove unnecessary definitions of installer and uninstaller

--- src/ModuleServiceProvider.php
+++ src/ModuleServiceProvider.php
@@ -9,34 +9,12 @@
 namespace Notadd\Mall;
 
 use Notadd\Foundation\Module\Abstracts\Module;
-use Notadd\Mall\Injections\Installer;
-use Notadd\Mall\Injections\Uninstaller;
 
 /**
  * Class ModuleServiceProvider.
  */
 class ModuleServiceProvider extends Module
 {
-    /**
-     * Install for module.
-     *
-     * @return string
-     */
-    public static function install()
-    {
-        return Installer::class;
-    }
-
-    /**
-     * Uninstall for module.
-     *
-     * @return string
-     */
-    public static function uninstall()
-    {
-        return Uninstaller::class;
-    }
-
     /**
      * Boot module.
      */
